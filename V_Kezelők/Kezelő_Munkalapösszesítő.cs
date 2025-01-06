67382a020b5d75693daab9d2e14f3006cc0d7130
István Pozsgai <istvanpozsgai72@gmail.com>
2025-01-06 12:46:36 +0100

    Munkalap összesítő OOP kezdet

---
+++ V_Kezelők/Kezelő_Munkalapösszesítő.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Data.OleDb;
+using Villamos.Villamos_Adatszerkezet;
+
+namespace Villamos.Villamos.Kezelők
+{
+    public class Kezelő_Munkalapösszesítő
+    {
+        public List<Adat_Munkalapösszesítő> Lista_Adatok(string hely, string jelszó, string szöveg)
+        {
+            List<Adat_Munkalapösszesítő> Adatok = new List<Adat_Munkalapösszesítő>();
+            Adat_Munkalapösszesítő Adat;
+
+            string kapcsolatiszöveg = $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source='{hely}'; Jet Oledb:Database Password={jelszó}";
+            using (OleDbConnection Kapcsolat = new OleDbConnection(kapcsolatiszöveg))
+            {
+                Kapcsolat.Open();
+                using (OleDbCommand Parancs = new OleDbCommand(szöveg, Kapcsolat))
+                {
+                    using (OleDbDataReader rekord = Parancs.ExecuteReader())
+                    {
+                        if (rekord.HasRows)
+                        {
+                            while (rekord.Read())
+                            {
+                                Adat = new Adat_Munkalapösszesítő(
+                                          rekord["ID"].ToÉrt_Long(),
+                                          rekord["megnevezés"].ToString(),
+                                          rekord["művelet"].ToString(),
+                                          rekord["pályaszám"].ToString(),
+                                          rekord["rendelés"].ToString()
+                                          );
+                                Adatok.Add(Adat);
+                            }
+                        }
+                    }
+                }
+            }
+            return Adatok;
+        }
+    }
+
+
+}
