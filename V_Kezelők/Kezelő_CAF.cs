5c4ab8db7cba8b21c01053599837871a6ef341e5
IstvanPozsgai <istvanpozsgai72@gmail.com>
2025-03-07 17:33:39 +0100

    Home Caf alapadat

--- V_Kezelők/Kezelő_CAF.cs
+++ V_Kezelők/Kezelő_CAF.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.OleDb;
 using System.Windows.Forms;
 using Villamos.Villamos_Adatszerkezet;
+using MyA = Adatbázis;
 
 namespace Villamos.Kezelők
 {
@@ -11,9 +12,14 @@ namespace Villamos.Kezelők
         readonly string hely = $@"{Application.StartupPath}\Főmérnökség\adatok\CAF\CAF.mdb";
         readonly string jelszó = "CzabalayL";
 
-        public List<Adat_CAF_alap> Lista_Adatok()
+        public List<Adat_CAF_alap> Lista_Adatok(bool Aktív = true)
         {
-            string szöveg = $"SELECT * FROM alap ORDER BY azonosító";
+            string szöveg;
+            if (Aktív)
+                szöveg = "SELECT * FROM alap WHERE törölt=false ORDER BY azonosító";
+            else
+                szöveg = $"SELECT * FROM alap ORDER BY azonosító";
+
             List<Adat_CAF_alap> Adatok = new List<Adat_CAF_alap>();
             Adat_CAF_alap Adat;
 
@@ -154,6 +160,102 @@ namespace Villamos.Kezelők
         }
 
 
+        public void Rögzítés(Adat_CAF_alap Adat)
+        {
+            try
+            {
+                // új jármű
+                string szöveg = "INSERT INTO alap (azonosító, Ciklusnap, Utolsó_Nap, Utolsó_Nap_sorszám, Végezte_nap, Vizsgdátum_nap, Cikluskm, Utolsó_Km,";
+                szöveg += " Utolsó_Km_sorszám, Végezte_km, Vizsgdátum_km, számláló, havikm, KMUkm, KMUdátum, fudátum, Teljeskm, Garancia, törölt, típus ) VALUES (";
+                szöveg += $"'{Adat.Azonosító}', "; // azonosító
+                szöveg += $"'{Adat.Ciklusnap}', "; // Ciklusnap
+                szöveg += $"'{Adat.Utolsó_Nap}', "; // Utolsó_Nap
+                szöveg += $"{Adat.Utolsó_Nap_sorszám}, "; // Utolsó_Nap_sorszám
+                szöveg += $"'{Adat.Végezte_nap}', "; // Végezte_nap
+                szöveg += $"'{Adat.Vizsgdátum_nap:MM-dd-yyyy}', "; // Vizsgdátum_nap
+                szöveg += $"'{Adat.Cikluskm}', "; // Cikluskm
+                szöveg += $"'{Adat.Utolsó_Km}', ";  // Utolsó_Km
+                szöveg += $"{Adat.Utolsó_Km_sorszám}, "; // Utolsó_Km_sorszám
+                szöveg += $"'{Adat.Végezte_km}', ";  // Végezte_km
+                szöveg += $"'{Adat.Vizsgdátum_km}', ";// Vizsgdátum_km
+                szöveg += $"{Adat.Számláló}, "; // számláló,
+                szöveg += $"{Adat.Havikm}, "; // havikm,
+                szöveg += $"{Adat.KMUkm}, ";  // KMUkm
+                szöveg += $"'{Adat.KMUdátum:MM-dd-yyyy}', "; // KMUdátum,
+                szöveg += $"'{Adat.Fudátum:MM-dd-yyyy}', "; // fudátum
+                szöveg += $"{Adat.Teljeskm}, "; // Teljeskm
+                szöveg += $"{Adat.Garancia}, ";     // Garancia
+                szöveg += $"{Adat.Törölt}, ";     // törölt
+                szöveg += $"'{Adat.Típus}')";
+                MyA.ABMódosítás(hely, jelszó, szöveg);
+            }
+            catch (HibásBevittAdat ex)
+            {
+                MessageBox.Show(ex.Message, "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                HibaNapló.Log(ex.Message, this.ToString(), ex.StackTrace, ex.Source, ex.HResult);
+                MessageBox.Show(ex.Message + "\n\n a hiba naplózásra került.", "A program hibára futott", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void Módosítás(Adat_CAF_alap Adat)
+        {
+            try
+            {
+                string szöveg = "UPDATE alap  SET ";
+                szöveg += $"Ciklusnap='{Adat.Ciklusnap}', "; // Ciklusnap
+                szöveg += $"Utolsó_Nap='{Adat.Utolsó_Nap}', "; // Utolsó_Nap
+                szöveg += $"Utolsó_Nap_sorszám={Adat.Utolsó_Nap_sorszám}, "; // Utolsó_Nap_sorszám
+                szöveg += $"Végezte_nap='{Adat.Végezte_nap}', "; // Végezte_nap
+                szöveg += $"Vizsgdátum_nap='{Adat.Vizsgdátum_nap:MM-dd-yyyy}', "; // Vizsgdátum_nap
+                szöveg += $"Cikluskm='{Adat.Cikluskm}', "; // Cikluskm
+                szöveg += $"Utolsó_Km='{Adat.Utolsó_Km}', ";  // Utolsó_Km
+                szöveg += $"Utolsó_Km_sorszám={Adat.Utolsó_Km_sorszám}, "; // Utolsó_Km_sorszám
+                szöveg += $"Végezte_km='{Adat.Végezte_km}', "; // Végezte_km
+                szöveg += $"Vizsgdátum_km='{Adat.Vizsgdátum_km:MM-dd-yyyy}', "; // Vizsgdátum_km
+                szöveg += $"számláló={Adat.Számláló}, "; // számláló,
+                szöveg += $"havikm={Adat.Havikm}, "; // havikm,
+                szöveg += $"KMUkm={Adat.KMUkm}, ";  // KMUkm
+                szöveg += $"KMUdátum='{Adat.KMUdátum:MM-dd-yyyy}', "; // KMUdátum,
+                szöveg += $"fudátum='{Adat.Fudátum:MM-dd-yyyy}', ";  // fudátum
+                szöveg += $"Teljeskm={Adat.Teljeskm}, "; // Teljeskm
+                szöveg += $"Garancia={Adat.Garancia}, ";  // Garancia
+                szöveg += $"törölt={Adat.Törölt}, ";// törölt
+                szöveg += $"típus='{Adat.Típus} '"; // típus
+                szöveg += $" WHERE azonosító='{Adat.Azonosító}'";
+                MyA.ABMódosítás(hely, jelszó, szöveg);
+            }
+            catch (HibásBevittAdat ex)
+            {
+                MessageBox.Show(ex.Message, "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                HibaNapló.Log(ex.Message, this.ToString(), ex.StackTrace, ex.Source, ex.HResult);
+                MessageBox.Show(ex.Message + "\n\n a hiba naplózásra került.", "A program hibára futott", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        public void Törlés(Adat_CAF_alap Adat)
+        {
+            try
+            {
+
+
+            }
+            catch (HibásBevittAdat ex)
+            {
+                MessageBox.Show(ex.Message, "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                HibaNapló.Log(ex.Message, this.ToString(), ex.StackTrace, ex.Source, ex.HResult);
+                MessageBox.Show(ex.Message + "\n\n a hiba naplózásra került.", "A program hibára futott", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 
     public class Kezelő_CAF_Adatok
