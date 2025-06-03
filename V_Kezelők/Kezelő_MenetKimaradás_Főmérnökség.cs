6cdabeb8155e2849137adf136ab426255b4c40e7
IstvanPozsgai <istvanpozsgai72@gmail.com>
2025-06-03 17:07:57 +0200

    módosítás

---
+++ V_Kezelők/Kezelő_MenetKimaradás_Főmérnökség.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Data.OleDb;
+using Villamos.Villamos_Adatszerkezet;
+
+namespace Villamos.Kezelők
+{
+    public class Kezelő_MenetKimaradás_Főmérnökség
+    {
+        public List<Adat_Menetkimaradás_Főmérnökség> Lista_Adatok(string hely, string jelszó, string szöveg)
+        {
+            List<Adat_Menetkimaradás_Főmérnökség> Adatok = new List<Adat_Menetkimaradás_Főmérnökség>();
+            Adat_Menetkimaradás_Főmérnökség Adat;
+
+            string kapcsolatiszöveg = $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source='{hely}'; Jet Oledb:Database Password={jelszó}";
+            using (OleDbConnection Kapcsolat = new OleDbConnection(kapcsolatiszöveg))
+            {
+                Kapcsolat.Open();
+                using (OleDbCommand Parancs = new OleDbCommand(szöveg, Kapcsolat))
+                {
+                    using (OleDbDataReader rekord = Parancs.ExecuteReader())
+                    {
+                        if (rekord.HasRows)
+                        {
+                            while (rekord.Read())
+                            {
+                                Adat = new Adat_Menetkimaradás_Főmérnökség(
+                                    rekord["viszonylat"].ToStrTrim(),
+                                    rekord["Azonosító"].ToStrTrim(),
+                                    rekord["típus"].ToStrTrim(),
+                                    rekord["eseményjele"].ToStrTrim(),
+                                    rekord["bekövetkezés"].ToÉrt_DaTeTime(),
+                                    rekord["kimaradtmenet"].ToÉrt_Long(),
+                                    rekord["jvbeírás"].ToStrTrim(),
+                                    rekord["vmbeírás"].ToStrTrim(),
+                                    rekord["javítás"].ToStrTrim(),
+                                    rekord["id"].ToÉrt_Long(),
+                                    rekord["törölt"].ToÉrt_Bool(),
+                                    rekord["jelentés"].ToStrTrim(),
+                                    rekord["tétel"].ToÉrt_Long(),
+                                    rekord["telephely"].ToStrTrim(),
+                                    rekord["szolgálat"].ToStrTrim()
+                                    );
+                                Adatok.Add(Adat);
+                            }
+                        }
+                    }
+                }
+            }
+            return Adatok;
+        }
+
+        public Adat_Menetkimaradás_Főmérnökség Egy_Adat(string hely, string jelszó, string szöveg)
+        {
+            Adat_Menetkimaradás_Főmérnökség Adat = null;
+
+            string kapcsolatiszöveg = $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source='{hely}'; Jet Oledb:Database Password={jelszó}";
+            using (OleDbConnection Kapcsolat = new OleDbConnection(kapcsolatiszöveg))
+            {
+                Kapcsolat.Open();
+                using (OleDbCommand Parancs = new OleDbCommand(szöveg, Kapcsolat))
+                {
+                    using (OleDbDataReader rekord = Parancs.ExecuteReader())
+                    {
+                        if (rekord.HasRows)
+                        {
+                            while (rekord.Read())
+                            {
+                                Adat = new Adat_Menetkimaradás_Főmérnökség(
+                                    rekord["viszonylat"].ToStrTrim(),
+                                    rekord["Azonosító"].ToStrTrim(),
+                                    rekord["típus"].ToStrTrim(),
+                                    rekord["eseményjele"].ToStrTrim(),
+                                    rekord["bekövetkezés"].ToÉrt_DaTeTime(),
+                                    rekord["kimaradtmenet"].ToÉrt_Long(),
+                                    rekord["jvbeírás"].ToStrTrim(),
+                                    rekord["vmbeírás"].ToStrTrim(),
+                                    rekord["javítás"].ToStrTrim(),
+                                    rekord["id"].ToÉrt_Long(),
+                                    rekord["törölt"].ToÉrt_Bool(),
+                                    rekord["jelentés"].ToStrTrim(),
+                                    rekord["tétel"].ToÉrt_Long(),
+                                    rekord["telephely"].ToStrTrim(),
+                                    rekord["szolgálat"].ToStrTrim()
+                                    );
+                            }
+                        }
+                    }
+                }
+            }
+            return Adat;
+        }
+    }
+}
