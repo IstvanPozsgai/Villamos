684bafce5f2e9695a88f633a640c32fc4a7bd0a5
BozaiMartin <bozaimartin@gmail.com>
2025-07-03 14:00:18 +0200

    1 kimaradt

--- V_Ablakok/4_Nyilvántartások/Eszterga_Karbantartás/Ablak_Eszterga_Karbantartás.cs
+++ V_Ablakok/4_Nyilvántartások/Eszterga_Karbantartás/Ablak_Eszterga_Karbantartás.cs
@@ -600,7 +600,7 @@ namespace Villamos.Villamos_Ablakok._5_Karbantartás.Eszterga_Karbantartás
                         DateTime.Today));
                 }
 
-                Kez_Muvelet_Naplo.Rogzites(naploLista);
+                Kez_Muvelet_Naplo.Rogzites(naploLista, DtmPckrElőTerv.Value.Year);
             }
             catch (HibásBevittAdat ex)
             {
