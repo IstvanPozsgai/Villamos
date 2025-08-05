199838c1da829d5e47d083172667b6ab126f199a
István Pozsgai <istvanpozsgai72@gmail.com>
2025-08-05 19:31:27 +0200

    Elkoptatott lista törlése

--- V_Kezelők/Kezelő_Épület_Naptár.cs
+++ V_Kezelők/Kezelő_Épület_Naptár.cs
@@ -76,38 +76,6 @@ namespace Villamos.Kezelők
         }
 
         //elkopó
-        public List<Adat_Épület_Naptár> Lista_Adatok(string hely, string jelszó, string szöveg)
-        {
-            List<Adat_Épület_Naptár> Adatok = new List<Adat_Épület_Naptár>();
-            Adat_Épület_Naptár Adat;
-
-            string kapcsolatiszöveg = $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source='{hely}'; Jet Oledb:Database Password={jelszó}";
-            using (OleDbConnection Kapcsolat = new OleDbConnection(kapcsolatiszöveg))
-            {
-                Kapcsolat.Open();
-                using (OleDbCommand Parancs = new OleDbCommand(szöveg, Kapcsolat))
-                {
-                    using (OleDbDataReader rekord = Parancs.ExecuteReader())
-                    {
-                        if (rekord.HasRows)
-                        {
-                            while (rekord.Read())
-                            {
-                                Adat = new Adat_Épület_Naptár(
-                                          rekord["Előterv"].ToÉrt_Bool(),
-                                          rekord["Hónap"].ToÉrt_Int(),
-                                          rekord["Igazolás"].ToÉrt_Bool(),
-                                          rekord["Napok"].ToStrTrim()
-                                          );
-
-                                Adatok.Add(Adat);
-                            }
-                        }
-                    }
-                }
-            }
-            return Adatok;
-        }
 
         public Adat_Épület_Naptár Egy_Adat(string hely, string jelszó, string szöveg)
         {
