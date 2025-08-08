890fa883e7944ba81273498f0dfaf876f06358bc
BozaiMartin <bozaimartin@gmail.com>
2025-08-08 08:26:57 +0200

    Eszterga hiba kommentek torlese

--- V_Ablakok/4_Nyilvántartások/Eszterga_Karbantartás/Ablak_Eszterga_Karbantartás_Napló.cs
+++ V_Ablakok/4_Nyilvántartások/Eszterga_Karbantartás/Ablak_Eszterga_Karbantartás_Napló.cs
@@ -14,7 +14,6 @@ namespace Villamos.V_Ablakok._4_Nyilvántartások.Eszterga_Karbantartás
     public partial class Ablak_Eszterga_Karbantartás_Napló : Form
     {
         public delegate void Event_Kidobo();
-        //hiba a rogzitesnel uzemorat rogziti amikor nem is kellene, tovabb megy amikor ki kene dobnia. 
         //databindingot elhanyagolni mas mod a szinezesre keresni egy masik oldalon 
         //ugyanezek a modosit oldalra
         #region Osztalyszintű elemek
