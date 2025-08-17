8595a669dc1de4d365fd360e962ab08cc2a07bc2
István Pozsgai <istvanpozsgai72@gmail.com>
2025-08-17 11:48:10 +0200

    Jogosultság beállítás további állítása

--- V_MindenEgyéb/GombLathatosagKezelo.cs
+++ V_MindenEgyéb/GombLathatosagKezelo.cs
@@ -5,32 +5,36 @@ using System.Windows.Forms;
 using Villamos;
 using Villamos.Adatszerkezet;
 using Villamos.Kezelõk;
+using Villamos.Villamos_Adatszerkezet;
 
 
 public static class GombLathatosagKezelo
 {
-
+    private static Kezelõ_Gombok KézGombok = new Kezelõ_Gombok();
     /// <summary>
     /// Beállítja az ablakon található gombok láthatóságát az adatbázis alapján.
     /// </summary>
     /// <param name="form">Az ablak (Form), amin a gombok találhatók.</param>
-    public static void Beallit(Form form)
+    public static void Beallit(Form form, string Telephely = "")
     {
-        GombokÁltalános(form);
-        GombokSzemélyes(form);
+        // Lekérjük az adott ablakhoz tartozó gombokat az adatbázisból
+        List<Adat_Gombok> gombok = KézGombok.Lista_Adatok()
+            .Where(g => g.FromName == form.Name && !g.Törölt)
+            .ToList();
+
+        GombokÁltalános(form, gombok);
+        GombokSzemélyes(form, gombok, Telephely);
     }
 
-    private static void GombokÁltalános(Form form)
+    /// <summary>
+    /// Kikapcsoljuk az ablakon található gombokat, ha a gombok adatbázisban megtalálhatóak, így nem lesznek láthatóak.
+    /// </summary>
+    /// <param name="form"></param>
+    /// <param name="gombok"></param>
+    private static void GombokÁltalános(Form form, List<Adat_Gombok> gombok)
     {
         try
         {
-            Kezelõ_Gombok kezeloGombok = new Kezelõ_Gombok();
-
-            // Lekérjük az adott ablakhoz tartozó gombokat az adatbázisból
-            List<Adat_Gombok> gombok = kezeloGombok.Lista_Adatok()
-                .Where(g => g.FromName == form.Name && !g.Törölt)
-                .ToList();
-
             foreach (Adat_Gombok adatGomb in gombok)
             {
                 // Megkeressük a gombot az ablak Controls gyûjteményében
@@ -40,7 +44,6 @@ public static class GombLathatosagKezelo
                     button.Visible = adatGomb.Látható;
                 }
             }
-
         }
         catch (HibásBevittAdat ex)
         {
@@ -53,39 +56,51 @@ public static class GombLathatosagKezelo
         }
     }
 
-    private static void GombokSzemélyes(Form form)
+    /// <summary>
+    /// Visszakapcsoljuk a gombokat, ha a felhasználónak van jogosultsága az adott gombhoz, az adott telephelyen.
+    /// </summary>
+    /// <param name="form"></param>
+    /// <param name=""></param>
+    /// <param name="gombok"></param>
+    /// <param name="Telephely"></param>
+    private static void GombokSzemélyes(Form form, List<Adat_Gombok> gombok, string Telephely = "")
     {
         try
         {
-            Kezelõ_Jogosultságok Kéz = new Kezelõ_Jogosultságok();
 
-            Kezelõ_Gombok kezeloGombok = new Kezelõ_Gombok();
-            // Lekérjük az adott ablakhoz tartozó gombokat az adatbázisból
-            List<Adat_Gombok> AdatokGombok = kezeloGombok.Lista_Adatok()
-                .Where(g => g.FromName == form.Name && !g.Törölt)
-                .ToList();
+            Kezelõ_Kiegészítõ_Könyvtár kézKönyvtár = new Kezelõ_Kiegészítõ_Könyvtár();
+            List<Adat_Kiegészítõ_Könyvtár> AdatokKönyvtár = kézKönyvtár.Lista_Adatok();
+            int TelephelyID = 0;
+            Adat_Kiegészítõ_Könyvtár TelephelyAdat = (from a in AdatokKönyvtár
+                                                      where a.Név == Telephely
+                                                      select a).FirstOrDefault();
+            if (TelephelyAdat != null) TelephelyID = TelephelyAdat.ID;
 
             // Lekérjük az aktuális oldal ID-ját
             Kezelõ_Oldalok KézOldal = new Kezelõ_Oldalok();
-            Adat_Oldalak AdatOldal = KézOldal.Lista_Adatok()
-                .Where(o => o.FromName == form.Name).FirstOrDefault();
+            Adat_Oldalak AdatOldal = KézOldal.Lista_Adatok().Where(o => o.FromName == form.Name).FirstOrDefault();
             if (AdatOldal == null) return;
 
             // Lekérjük az adott felhasználóhoz tartozó gombokat az adatbázisból
-            List<Adat_Jogosultságok> jogosultságok = Kéz.Lista_Adatok()
-                .Where(j => j.UserId == Program.PostásNévId && !j.Törölt && j.OldalId == AdatOldal.OldalId)
-                .ToList();
+            List<Adat_Jogosultságok> jogosultságok = Program.PostásJogosultságok;
+            jogosultságok = (from j in jogosultságok
+                             where j.UserId == Program.PostásNévId
+                             && !j.Törölt
+                             && j.SzervezetId == TelephelyID
+                             && j.OldalId == AdatOldal.OldalId
+                             select j).ToList();
+
             // ha a jogosultáság táblában van akkor van hozzá joga így láthatóvá tesszük a gombokat
             foreach (Adat_Jogosultságok adatGomb in jogosultságok)
             {
-                Adat_Gombok Egygomb = (from a in AdatokGombok
+                Adat_Gombok Egygomb = (from a in gombok
                                        where a.GombokId == adatGomb.GombokId
                                        select a).FirstOrDefault();
                 if (Egygomb != null)
-                {                // Megkeressük a gombot az ablak Controls gyûjteményében
+                {
+                    // Megkeressük a gombot az ablak Controls gyûjteményében
                     Control control = form.Controls.Find(Egygomb.GombName, true).FirstOrDefault();
                     if (control is Button button) button.Visible = true;
-
                 }
             }
         }
