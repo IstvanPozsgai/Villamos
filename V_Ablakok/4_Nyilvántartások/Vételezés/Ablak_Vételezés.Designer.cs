d6ee53b5720fc51b6dafb212b28f736c6387c370
István Pozsgai <istvanpozsgai72@gmail.com>
2025-09-12 16:44:38 +0200

    Új könyvtárba áttétel előkészítés

--- V_Ablakok/4_Nyilvántartások/Vételezés/Ablak_Vételezés.Designer.cs
+++ V_Ablakok/4_Nyilvántartások/Vételezés/Ablak_Vételezés.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(734, 350);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Ablak_Vételezés";
