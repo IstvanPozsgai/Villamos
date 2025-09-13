043aea36f254830fbf935cf514696949383beed9
István Pozsgai <istvanpozsgai72@gmail.com>
2025-09-13 07:55:20 +0200

    Új ablak létrehozása

---
+++ V_Ablakok/4_Nyilvántartások/Vételezés/Ablak_Anyag_Karbantartás.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Villamos.V_Ablakok._4_Nyilvántartások.Vételezés
+{
+    public partial class Ablak_Anyag_Karbantartás : Form
+    {
+        public Ablak_Anyag_Karbantartás()
+        {
+            InitializeComponent();
+        }
+    }
+}
