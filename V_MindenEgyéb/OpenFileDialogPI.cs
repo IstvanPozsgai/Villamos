6e322072363d24dc1d186b1cfea7828bcc5db7a4
PapRichard <richard_pap@stud.uni-obuda.hu>
2025-09-19 10:34:25 +0200

    Excel fajl tallozasakor bezarja a nyitott tallozott munkafuzetet.

--- V_MindenEgyéb/OpenFileDialogPI.cs
+++ V_MindenEgyéb/OpenFileDialogPI.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Villamos.V_MindenEgyéb
@@ -26,20 +28,55 @@ namespace Villamos.V_MindenEgyéb
                 result = dialog.ShowDialog();
                 if (result != DialogResult.OK) return result;
 
-                string FájlKiterjesztés = Path.GetExtension(dialog.FileName).TrimStart('.').ToLower();
-                if (string.IsNullOrEmpty(FájlKiterjesztés))
+                string fájlKiterjesztés = Path.GetExtension(dialog.FileName).TrimStart('.').ToLower();
+                if (string.IsNullOrEmpty(fájlKiterjesztés))
                 {
-                    MessageBox.Show("Nincs a betölteni kívánt fájlnak fájl kiterjesztése!", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Nincs a betölteni kívánt fájlnak kiterjesztése!", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     continue;
                 }
-                if (!KiterjesztésLista.Contains(FájlKiterjesztés))
+                if (!KiterjesztésLista.Contains(fájlKiterjesztés))
                 {
-                    MessageBox.Show($"Nem megfelelõ a betölteni kívánt fájl kiterjesztés:{FájlKiterjesztés} formátuma!", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show($"Nem megfelelõ a betölteni kívánt fájl kiterjesztés: {fájlKiterjesztés}!", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     continue;
                 }
+
+                // Ellenõrzés + bezárás elérési út alapján Excel esetén
+                // Mint utólag rájöttem, a FileName nem a fájlnevet, hanem a teljes elérési utat adja vissza.
+                if (KiterjesztésLista.Contains(fájlKiterjesztés))
+                {
+                    IsFileOpened(dialog.FileName);
+                }
+               
                 isValidFile = true;
             }
             return result;
+        }
+
+        // Bezárja a tallózott Excel táblát abban az esetben, ha az nyitva van.
+        static void IsFileOpened(string filePath)
+        {
+            try
+            {
+                // Kapcsolódás a futó Excel példányhoz
+                Microsoft.Office.Interop.Excel.Application excelApp =
+                    (Microsoft.Office.Interop.Excel.Application)Marshal.GetActiveObject("Excel.Application");
+
+                // Megkeressük a keresett fájlt teljes elérési út alapján
+                var workbook = excelApp.Workbooks
+                    .Cast<Microsoft.Office.Interop.Excel.Workbook>()
+                    .FirstOrDefault(wb => wb.FullName.Equals(filePath, StringComparison.OrdinalIgnoreCase));
+
+                if (workbook != null)
+                {
+                    workbook.Close(true); // true = mentés bezárás elõtt, nem kérdez
+                    Marshal.ReleaseComObject(workbook);
+                    // sikerült bezárni
+                }
+            }
+            catch (COMException)
+            {
+                // Ha nincs futó Excel példány nem akad fent.
+            }
         }
     }
 }
