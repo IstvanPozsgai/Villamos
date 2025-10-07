b069f27958dc23b7ab5b7c0ced429a8c8a352666
Pap Richard <168770140+clanishcoffee@users.noreply.github.com>
2025-10-07 10:14:41 +0200

    Hibanapó ZUBY tábla felépítésének elkezdése.

--- V_MindenEgyéb/Ablak_Hibanaplo.Designer.cs
+++ V_MindenEgyéb/Ablak_Hibanaplo.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Hibanaplo_Tablazat = new Zuby.ADGV.AdvancedDataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.Hibanaplo_Tablazat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Hibanaplo_Tablazat
+            // 
+            this.Hibanaplo_Tablazat.AllowUserToAddRows = false;
+            this.Hibanaplo_Tablazat.AllowUserToDeleteRows = false;
+            this.Hibanaplo_Tablazat.AllowUserToOrderColumns = true;
+            this.Hibanaplo_Tablazat.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Hibanaplo_Tablazat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Hibanaplo_Tablazat.FilterAndSortEnabled = true;
+            this.Hibanaplo_Tablazat.FilterStringChangedInvokeBeforeDatasourceUpdate = true;
+            this.Hibanaplo_Tablazat.Location = new System.Drawing.Point(0, 0);
+            this.Hibanaplo_Tablazat.MaxFilterButtonImageHeight = 23;
+            this.Hibanaplo_Tablazat.Name = "Hibanaplo_Tablazat";
+            this.Hibanaplo_Tablazat.ReadOnly = true;
+            this.Hibanaplo_Tablazat.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Hibanaplo_Tablazat.RowHeadersWidth = 51;
+            this.Hibanaplo_Tablazat.RowTemplate.Height = 24;
+            this.Hibanaplo_Tablazat.Size = new System.Drawing.Size(800, 450);
+            this.Hibanaplo_Tablazat.SortStringChangedInvokeBeforeDatasourceUpdate = true;
+            this.Hibanaplo_Tablazat.TabIndex = 0;
+            // 
+            // Ablak_Hibanaplo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Hibanaplo_Tablazat);
+            this.Name = "Ablak_Hibanaplo";
             this.Text = "Ablak_Hibanaplo";
+            this.Load += new System.EventHandler(this.Ablak_Hibanaplo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Hibanaplo_Tablazat)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Zuby.ADGV.AdvancedDataGridView Hibanaplo_Tablazat;
     }
 }
--- V_MindenEgyéb/Ablak_Hibanaplo.cs
+++ V_MindenEgyéb/Ablak_Hibanaplo.cs
@@ -12,9 +12,56 @@ namespace Villamos.V_MindenEgyéb
 {
     public partial class Ablak_Hibanaplo : Form
     {
+
+        readonly DataTable AdatTábla = new DataTable();
+
+        // Dátum;Telephely;Felhsználó;Hiba üzenet;Hiba Osztály; Hiba Metódus; Névtér; Egyéb; Dátum
         public Ablak_Hibanaplo()
         {
             InitializeComponent();
         }
+
+        private void Ablak_Hibanaplo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Start()
+        {
+
+        }
+
+        private void Tablalista_kiírás()
+        {
+            Listazas();
+            ABFeltöltése();
+            Hibanaplo_Tablazat.CleanFilterAndSort();
+            Hibanaplo_Tablazat.DataSource = AdatTábla;
+            OszlopSzélesség();
+            Hibanaplo_Tablazat.Refresh();
+            Hibanaplo_Tablazat.Visible = true;
+            Hibanaplo_Tablazat.ClearSelection();
+        }
+
+        private void Fejlec()
+        {
+           
+        }
+
+        private void Listazas()
+        {
+
+        }
+
+        private void ABFeltöltése()
+        {
+
+        }
+
+        private void OszlopSzélesség()
+        {
+
+        }
+
     }
 }
