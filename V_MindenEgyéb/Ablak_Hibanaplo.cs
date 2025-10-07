92f1d9758a92438c33521bbc6616cb322226c4c1
Pap Richard <168770140+clanishcoffee@users.noreply.github.com>
2025-10-07 10:18:04 +0200

    Hibanapló tábla fejléc elkészítése

--- V_MindenEgyéb/Ablak_Hibanaplo.cs
+++ V_MindenEgyéb/Ablak_Hibanaplo.cs
@@ -14,11 +14,11 @@ namespace Villamos.V_MindenEgyéb
     {
 
         readonly DataTable AdatTábla = new DataTable();
-
-        // Dátum;Telephely;Felhsználó;Hiba üzenet;Hiba Osztály; Hiba Metódus; Névtér; Egyéb; Dátum
+        
         public Ablak_Hibanaplo()
         {
             InitializeComponent();
+            Start();
         }
 
         private void Ablak_Hibanaplo_Load(object sender, EventArgs e)
@@ -28,7 +28,7 @@ namespace Villamos.V_MindenEgyéb
 
         private void Start()
         {
-
+            Fejlec();
         }
 
         private void Tablalista_kiírás()
@@ -45,7 +45,16 @@ namespace Villamos.V_MindenEgyéb
 
         private void Fejlec()
         {
-           
+            // Dátum;Telephely;Felhsználó;Hiba üzenet;Hiba Osztály; Hiba Metódus; Névtér; Egyéb; Dátum
+            AdatTábla.Columns.Clear();
+            AdatTábla.Columns.Add("Dátum", typeof(DateTime));
+            AdatTábla.Columns.Add("Telephely");
+            AdatTábla.Columns.Add("Felhasználó");
+            AdatTábla.Columns.Add("Hiba üzenet");
+            AdatTábla.Columns.Add("Hiba osztály");
+            AdatTábla.Columns.Add("Hiba metódus");
+            AdatTábla.Columns.Add("Névtér");
+            AdatTábla.Columns.Add("Egyéb");
         }
 
         private void Listazas()
