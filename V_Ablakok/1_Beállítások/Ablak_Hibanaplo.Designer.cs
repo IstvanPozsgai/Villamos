f6ed1b3e2afec2f1ec715c78f6db22b91a858913
Pap Richard <168770140+clanishcoffee@users.noreply.github.com>
2025-10-08 06:27:02 +0200

    Hibanapló ablak évválasztó combobox.

--- V_Ablakok/1_Beállítások/Ablak_Hibanaplo.Designer.cs
+++ V_Ablakok/1_Beállítások/Ablak_Hibanaplo.Designer.cs
@@ -30,7 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ablak_Hibanaplo));
             this.Hibanaplo_Tablazat = new Zuby.ADGV.AdvancedDataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.cmb_korabbi_evek = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.Hibanaplo_Tablazat)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Hibanaplo_Tablazat
@@ -42,28 +45,52 @@
             this.Hibanaplo_Tablazat.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Hibanaplo_Tablazat.FilterAndSortEnabled = true;
             this.Hibanaplo_Tablazat.FilterStringChangedInvokeBeforeDatasourceUpdate = true;
-            this.Hibanaplo_Tablazat.Location = new System.Drawing.Point(0, 0);
+            this.Hibanaplo_Tablazat.Location = new System.Drawing.Point(3, 33);
             this.Hibanaplo_Tablazat.MaxFilterButtonImageHeight = 23;
             this.Hibanaplo_Tablazat.Name = "Hibanaplo_Tablazat";
             this.Hibanaplo_Tablazat.ReadOnly = true;
             this.Hibanaplo_Tablazat.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Hibanaplo_Tablazat.RowHeadersWidth = 51;
             this.Hibanaplo_Tablazat.RowTemplate.Height = 24;
-            this.Hibanaplo_Tablazat.Size = new System.Drawing.Size(800, 450);
+            this.Hibanaplo_Tablazat.Size = new System.Drawing.Size(794, 414);
             this.Hibanaplo_Tablazat.SortStringChangedInvokeBeforeDatasourceUpdate = true;
             this.Hibanaplo_Tablazat.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.Hibanaplo_Tablazat, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cmb_korabbi_evek, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // cmb_korabbi_evek
+            // 
+            this.cmb_korabbi_evek.FormattingEnabled = true;
+            this.cmb_korabbi_evek.Location = new System.Drawing.Point(3, 3);
+            this.cmb_korabbi_evek.Name = "cmb_korabbi_evek";
+            this.cmb_korabbi_evek.Size = new System.Drawing.Size(140, 24);
+            this.cmb_korabbi_evek.TabIndex = 1;
             // 
             // Ablak_Hibanaplo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Hibanaplo_Tablazat);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Ablak_Hibanaplo";
             this.Text = "Hibanapló";
             this.Load += new System.EventHandler(this.Ablak_Hibanaplo_Load);
             ((System.ComponentModel.ISupportInitialize)(this.Hibanaplo_Tablazat)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -71,5 +98,7 @@
         #endregion
 
         private Zuby.ADGV.AdvancedDataGridView Hibanaplo_Tablazat;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ComboBox cmb_korabbi_evek;
     }
 }
