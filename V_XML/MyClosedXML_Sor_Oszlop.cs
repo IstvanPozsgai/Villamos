027963003116e71764ddf70d3b074822e9b50322
Pozsgai István <117170572+IstvanPozsgai@users.noreply.github.com>
2025-11-27 18:46:46 +0100

    OszlopRejtés XML megvalósítása

--- V_XML/MyClosedXML_Sor_Oszlop.cs
+++ V_XML/MyClosedXML_Sor_Oszlop.cs
@@ -72,6 +72,28 @@ namespace Villamos
 
         }
 
+        /// <summary>
+        /// Elrejti az oszlopot
+        /// </summary>
+        /// <param name="oszlop"></param>
+        public static void OszlopRejtés(string munkalapNév, string oszlop)
+        {
+            try
+            {
+                IXLWorksheet munkalap = xlWorkBook.Worksheet(munkalapNév);
+                if (oszlop.Contains(':')) oszlop = oszlop.Split(':')[0].Trim();
+                if (munkalap == null) throw new ArgumentException($"A(z) '{munkalapNév}' nevű munkalap nem található.");
+                munkalap.Column(oszlop).Hide();
+            }
+            catch (Exception ex)
+            {
+                StackFrame hívó = new System.Diagnostics.StackTrace().GetFrame(1);
+                string hívóInfo = hívó?.GetMethod()?.DeclaringType?.FullName + "-" + hívó?.GetMethod()?.Name;
+                HibaNapló.Log(ex.Message, $"OszlopRejtés(munkalap {munkalapNév}, oszlop {oszlop}) \n Hívó: {hívóInfo}", ex.StackTrace, ex.Source, ex.HResult);
+                MessageBox.Show(ex.Message + "\n\n a hiba naplózásra került.", "A program hibára futott", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
         public static void Háttérszín(string munkalapnév, string mit, System.Drawing.Color színe)
         {
             try
