b93d64cdface2b622631cbf2880e3055b2bffa70
István Pozsgai <istvanpozsgai72@gmail.com>
2025-12-06 18:26:21 +0100

    a

---
+++ V_Adatszerkezet/Adat_Beallitas_Diagram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamos.V_Adatszerkezet
+{
+    public class Beállítás_Diagram
+    {
+        public string Munkalap { get; set; }
+        public int FelsőX { get; set; }
+        public int FelsőY { get; set; }
+        public string TáblaFelső { get; set; }
+        public string TáblaAlsó { get; set; }
+
+        public string DiagramCim { get; set; }
+    }
+
+}
