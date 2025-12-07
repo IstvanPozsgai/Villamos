ead875bbcaf37fd977327f6d79899e957283421d
István Pozsgai <istvanpozsgai72@gmail.com>
2025-12-07 11:00:16 +0100

    Beállítás_Kimutatás osztály létrehozása MASTER

---
+++ V_Adatszerkezet/Beállítás_Kimutatás.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Villamos.Adatszerkezet
+{
+    public class Beállítás_Kimutatás
+    {
+        /// <summary>
+        /// Az adatok munkalapjának a neve
+        /// </summary>
+        public string Munkalapnév { get; set; }
+        public string Balfelső { get; set; }
+        public string Jobbalsó { get; set; }
+
+        /// <summary>
+        /// Hova lesz rakva a kimutatás a munkalapot nem hozza létre
+        /// </summary>
+        public string Kimutatás_Munkalapnév { get; set; }
+        public string Kimutatás_cella { get; set; }
+        public string Kimutatás_név { get; set; }
+        public List<string> ÖsszesítNév { get; set; }
+        public List<string> Összesítés_módja { get; set; }
+        public List<string> SorNév { get; set; }
+        public List<string> OszlopNév { get; set; }
+        public List<string> SzűrőNév { get; set; }
+    }
+}
