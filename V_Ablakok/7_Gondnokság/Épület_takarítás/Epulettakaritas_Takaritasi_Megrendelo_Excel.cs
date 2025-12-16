0b1085643e60c7f0dc66a98a845a2bbe0a18cb1d
clanishcoffee <richard_pap@stud.uni-obuda.hu>
2025-12-16 09:09:44 +0100

    Épülettkarítás W.I.P

---
+++ V_Ablakok/7_Gondnokság/Épület_takarítás/Epulettakaritas_Takaritasi_Megrendelo_Excel.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Villamos.Villamos_Adatszerkezet;
+using MyX = Villamos.MyClosedXML_Excel;
+using MyF = Függvénygyűjtemény;
+
+namespace Villamos.V_Ablakok._7_Gondnokság.Épület_takarítás
+{
+    public class Epulettakaritas_Takaritasi_Megrendelo_Excel
+    {
+        public void ExcelKeszit()
+        {
+            string munkalap = "Munka1";
+            MyX.ExcelLétrehozás();
+            // megnyitjuk az excelt
+
+            //Holtart.Be();
+            // *********************************************
+            // ********* Osztály tábla *********************
+            // *********************************************
+            // fejléc elkészítése
+            MyX.Kiir("Megnevezés", "a1");
+            MyX.Kiir("E1 Egységár [Ft/m2]", "c1");
+            MyX.Kiir("E2 Egységár [Ft/m2]", "d1");
+            MyX.Kiir("E3 Egységár [Ft/m2]", "e1");
+
+            AdatokKijelöltek = KézTakarításrakijelölt.Lista_Adatok(Cmbtelephely.Text.Trim(), Dátum.Value.Year);
+            AdatokTakOsztály = KézOsztály.Lista_Adatok(Cmbtelephely.Text.Trim());
+            AdatokAdatTábla = KézAdatTábla.Lista_Adatok(Cmbtelephely.Text.Trim());
+
+            AdatokTakOsztály = (from a in AdatokTakOsztály
+                                where a.Státus == false
+                                orderby a.Id
+                                select a).ToList();
+            //Holtart.Be(20);
+
+            int sor = 2;
+            foreach (Adat_Épület_Takarítás_Osztály rekord in AdatokTakOsztály)
+            {
+                MyX.Kiir(rekord.Osztály.Trim(), "a" + sor.ToString());
+                MyX.Kiir(rekord.E1Ft.ToString().Replace(",", "."), "c" + sor.ToString());
+                MyX.Kiir(rekord.E2Ft.ToString().Replace(",", "."), "d" + sor.ToString());
+                MyX.Kiir(rekord.E3Ft.ToString().Replace(",", "."), "e" + sor.ToString());
+                //Holtart.Lép();
+                sor += 1;
+            }
+            MyX.Oszlopszélesség(munkalap, "A:A");
+            MyX.Oszlopszélesség(munkalap, "B:B");
+            MyX.Oszlopszélesség(munkalap, "C:E");
+            MyX.Rácsoz("a1:e" + (sor - 1).ToString());
+
+            MyX.Munkalap_átnevezés("Munka1", "Összesítő");
+            MyX.OszlopRejtés("Összesítő", "B:B");
+
+            MyX.Új_munkalap(Cmbtelephely.Text.Trim());
+            MyX.Munkalap_betű("Calibri", 10);
+            munkalap = Cmbtelephely.Text.Trim();
+            // ************************************************
+            // ************ fejléc elkészítése  ***************
+            // ************************************************
+            MyX.Egyesít(munkalap, "b1:b2");
+            MyX.Kiir("Helyiség", "b1");
+            MyX.Egyesít(munkalap, "c1:c2");
+            MyX.Kiir("Alapterület [m2]", "c1");
+            MyX.Egyesít(munkalap, "d1:k1");
+            MyX.Kiir("Gyakoriság", "d1");
+            MyX.Kiir("Szolgálatási jegyzék kódja", "d2");
+            MyX.Kiir("Szolgálatási jegyzék kódja", "g2");
+            MyX.Kiir("Szolgálatási jegyzék kódja", "j2");
+            MyX.Kiir("Gyakoriság alkalom /év", "e2");
+            MyX.Kiir("Gyakoriság alkalom /év", "h2");
+            MyX.Kiir("Gyakoriság alkalom /hó", "f2");
+            MyX.Kiir("Gyakoriság alkalom /hó", "i2");
+            MyX.Kiir("Gyakoriság alkalom /hó", "k2");
+            MyX.Egyesít(munkalap, "l1:l2");
+            MyX.Kiir("E1 Egységár [Ft/alkalom]", "l1");
+            MyX.Egyesít(munkalap, "m1:m2");
+            MyX.Kiir("E2 Egységár [Ft/alkalom]", "m1");
+            MyX.Egyesít(munkalap, "n1:n2");
+            MyX.Kiir("E3 Egységár [Ft/alkalom]", "n1");
+            MyX.Egyesít(munkalap, "o1:o2");
+            MyX.Kiir("E1 Egységár [Ft/hó]", "o1");
+            MyX.Egyesít(munkalap, "p1:p2");
+            MyX.Kiir("E2 Egységár [Ft/hó]", "p1");
+            MyX.Egyesít(munkalap, "q1:q2");
+            MyX.Kiir("E3 Egységár [Ft/hó]", "q1");
+            MyX.Egyesít(munkalap, "r1:r2");
+            MyX.Kiir("Összesen: [Ft/hó]", "r1");
+            MyX.Egyesít(munkalap, "s1:t2");
+            MyX.Kiir("Feladatellátás tervezett időpontja", "s1");
+            MyX.Egyesít(munkalap, "u1:w1");
+            MyX.Kiir("Minőségellenőrzésért, teljesítési igazolásért felelős személy", "u1");
+            MyX.Kiir("Neve", "u2");
+            MyX.Kiir("Telefonszám", "v2");
+            MyX.Kiir("E-mail cím", "w2");
+            MyX.Sormagasság("1:1", 47);
+            MyX.Sormagasság("2:2", 39);
+            MyX.Oszlopszélesség(munkalap, "B:B", 46);
+            MyX.Oszlopszélesség(munkalap, "c:k", 11);
+            MyX.Oszlopszélesség(munkalap, "l:n", 13);
+            MyX.Oszlopszélesség(munkalap, "o:v", 15);
+            MyX.Oszlopszélesség(munkalap, "w:W", 20);
+            MyX.Sortörésseltöbbsorba_egyesített("c1");
+            MyX.Sortörésseltöbbsorba("d2:k2");
+            MyX.Sortörésseltöbbsorba_egyesített("l1");
+            MyX.Sortörésseltöbbsorba_egyesített("m1");
+            MyX.Sortörésseltöbbsorba_egyesített("n1");
+            MyX.Sortörésseltöbbsorba_egyesített("o1");
+            MyX.Sortörésseltöbbsorba_egyesített("p1");
+            MyX.Sortörésseltöbbsorba_egyesített("r1");
+            MyX.OszlopRejtés(munkalap, "A:A");
+
+            // a táblázat érdemi része
+
+            sor = 2;
+            Adat_Épület_Takarításrakijelölt rekordép;
+
+            foreach (Adat_Épület_Takarítás_Osztály rekord in AdatokTakOsztály)
+            {
+                sor += 1;
+                MyX.Egyesít(munkalap, "b" + sor.ToString() + ":W" + sor.ToString());
+                MyX.Igazít_vízszintes("b" + sor.ToString() + ":W" + sor.ToString(), "bal");
+                MyX.Háttérszín("b" + sor.ToString() + ":W" + sor.ToString(), 13434828);
+                MyX.Kiir(rekord.Osztály.Trim(), "b" + sor.ToString());
+                MyX.Sormagasság(sor.ToString() + ":" + sor.ToString(), 20);
+
+                List<Adat_Épület_Adattábla> AdatokA = (from a in AdatokAdatTábla
+                                                       where a.Státus == false
+                                                       && a.Osztály == rekord.Osztály.Trim()
+                                                       orderby a.ID
+                                                       select a).ToList();
+
+                foreach (Adat_Épület_Adattábla rekord1 in AdatokA)
+                {
+                    sor++;
+                    MyX.Kiir(rekord1.Osztály.Trim(), "A" + sor.ToString());
+                    MyX.Kiir(rekord1.Megnevezés.Trim(), "b" + sor.ToString());
+                    MyX.Kiir(rekord1.Méret.ToString(), "c" + sor.ToString());
+                    MyX.Kiir("E1", "d" + sor.ToString());
+                    MyX.Kiir(rekord1.E1évdb.ToString(), "e" + sor.ToString());
+                    MyX.Kiir("E2", "g" + sor.ToString());
+                    MyX.Kiir(rekord1.E2évdb.ToString(), "h" + sor.ToString());
+                    MyX.Kiir("E3", "j" + sor.ToString());
+                    int idE1db = 0;
+                    int idE2db = 0;
+                    int idE3db = 0;
+
+                    rekordép = (from a in AdatokKijelöltek
+                                where a.Hónap == Dátum.Value.Month
+                                && a.Helységkód == rekord1.Helységkód.Trim()
+                                select a).FirstOrDefault();
+
+                    if (rekordép != null)
+                    {
+                        idE1db = rekordép.E1kijelöltdb;
+                        idE2db = rekordép.E2kijelöltdb;
+                        idE3db = rekordép.E3kijelöltdb;
+                    }
+
+                    MyX.Kiir(idE1db.ToString(), "f" + sor.ToString());
+                    MyX.Kiir(idE2db.ToString(), "i" + sor.ToString());
+                    MyX.Kiir(idE3db.ToString(), "k" + sor.ToString());
+                    MyX.Kiir(rekord.E1Ft.ToString().Replace(",", "."), "l" + sor.ToString()); //Ez darabra megy
+                    MyX.Kiir((rekord.E2Ft * rekord1.Méret).ToString().Replace(",", "."), "m" + sor.ToString());
+                    MyX.Kiir((rekord.E3Ft * rekord1.Méret).ToString().Replace(",", "."), "n" + sor.ToString());
+                    MyX.Kiir("=RC[-3]*RC[-9]", "o" + sor.ToString());
+                    MyX.Kiir("=RC[-3]*RC[-7]", "p" + sor.ToString());
+                    MyX.Kiir("=RC[-3]*RC[-6]", "q" + sor.ToString());
+                    MyX.Kiir("=SUM(RC[-3]:RC[-1])", "r" + sor.ToString());
+                    MyX.Kiir(rekord1.Kezd.Trim(), "s" + sor.ToString());
+                    MyX.Kiir(rekord1.Végez.Trim(), "t" + sor.ToString());
+                    MyX.Kiir(rekord1.Ellenőrneve.Trim(), "u" + sor.ToString());
+                    MyX.Kiir(rekord1.Ellenőrtelefonszám.Trim(), "v" + sor.ToString());
+                    MyX.Kiir(rekord1.Ellenőremail.Trim(), "w" + sor.ToString());
+                }
+                //Holtart.Lép();
+            }
+
+
+            // összesítő sor
+            sor += 1;
+            MyX.Igazít_vízszintes("b" + sor.ToString() + ":W" + sor.ToString(), "bal");
+            MyX.Háttérszín("b" + sor.ToString() + ":W" + sor.ToString(), 13434828);
+            MyX.Egyesít(munkalap, "b" + sor.ToString() + ":n" + sor.ToString());
+            MyX.Kiir(Cmbtelephely.Text.Trim() + " Összesen/hó", "b" + sor.ToString() + ":n" + sor.ToString());
+            MyX.Betű("b" + sor.ToString() + ":n" + sor.ToString(), false, false, true);
+            MyX.Egyesít(munkalap, "b" + sor.ToString() + ":n" + sor.ToString());
+            MyX.Egyesít(munkalap, "o" + sor.ToString() + ":r" + sor.ToString());
+            MyX.Kiir("=SUM(R[-" + (sor - 3).ToString() + "]C[3]:R[-1]C[3])", "o" + sor.ToString() + ":r" + sor.ToString());
+            MyX.Rácsoz("b1:W" + sor.ToString());
+            MyX.Sormagasság(sor.ToString() + ":" + sor.ToString(), 25);
+
+            // bezárjuk az Excel-t
+            MyX.Aktív_Cella(munkalap, "A1");
+            MyX.ExcelMentés(fájlexc);
+            MyX.ExcelBezárás();
+        }
+    }
+}
---
+++ V_Ablakok/7_Gondnokság/Épület_takarítás/Epulettakaritas_Takaritasi_Naplo_Excel.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using Villamos.V_Adatszerkezet;
+using Villamos.Villamos_Adatszerkezet;
+using MyF = Függvénygyűjtemény;
+using MyX = Villamos.MyClosedXML_Excel;
+using Villamos.Adatszerkezet;
+
+namespace Villamos.V_Ablakok._7_Gondnokság.Épület_takarítás
+{
+    public class Epulettakaritas_Takaritasi_Naplo_Excel
+    {
+        public void ExceltKeszit(string fájlexc, DateTime Dátum, List<Adat_Épület_Takarításrakijelölt> AdatokKijelöltek, List<Adat_Épület_Adattábla> AdatokAdatTábla, List<Adat_Épület_Naptár> AdatokÉNaptár, string helységkód, int hónapnap)
+        {
+            Beállítás_Betű BeBetű = new Beállítás_Betű() { Név= "Arial", Méret = 12};
+            Beállítás_Betű BeBetűVastag = new Beállítás_Betű() { Név = "Arial", Méret = 12, Vastag=true };
+            Beállítás_Betű BeBetűDőlt = new Beállítás_Betű() { Név = "Arial", Méret = 12, Dőlt = true };
+            string munkalap = "Munka1";
+            // megnyitjuk az excelt
+            MyX.ExcelLétrehozás();
+            MyX.Munkalap_betű(munkalap, BeBetű);
+            MyX.Sormagasság(munkalap,"1:50", 18);
+
+            // oszlop széleségek beállítása
+            MyX.Oszlopszélesség(munkalap, "a:n", 5);
+            MyX.Oszlopszélesség(munkalap, "a:a", 7);
+            MyX.Oszlopszélesség(munkalap, "e:f", 8);
+            MyX.Oszlopszélesség(munkalap, "g:g", 10);
+            MyX.Oszlopszélesség(munkalap, "j:k", 10);
+            MyX.Oszlopszélesség(munkalap, "n:n", 10);
+            // '**********************************************
+            // '**          fejléc          ******************
+            // '**********************************************
+            MyX.Egyesít(munkalap, "a1:N1");
+            MyX.Kiir(Dátum.ToString("yyyy MMMM"), "a1");
+            MyX.Betű(munkalap,"a1", BeBetűVastag);
+            MyX.Egyesít(munkalap, "a2:n4");
+            MyX.Kiir("helyiség", "a2");
+
+            MyX.Egyesít(munkalap, "a5:n5");
+            MyX.Kiir("Takarítási napló", "a5");
+            MyX.Betű(munkalap,"a5", BeBetűVastag);
+            MyX.Vastagkeret(munkalap,"a7");
+            MyX.Egyesít(munkalap, "b7:g7");
+            MyX.Vastagkeret(munkalap,"b7:g7");
+            MyX.Egyesít(munkalap, "h7:n7");
+            MyX.Vastagkeret(munkalap,"h7:n7");
+            MyX.Kiir("Szolgáltató tölti ki", "b7");
+            MyX.Betű(munkalap,"b7", BeBetűDőlt);
+            MyX.Kiir("BKV szervezeti igazolója tölti ki", "h7");
+            MyX.Betű(munkalap,"h7", BeBetűDőlt);
+            MyX.Sormagasság(munkalap,"8:8", 51);
+            MyX.Egyesít(munkalap, "a8:a9");
+            MyX.Kiir("Dátum", "a8");
+            MyX.Egyesít(munkalap, "b8:d8");
+            MyX.Kiir("Szolg. jegyzék kódja", "b8");
+            MyX.Sortörésseltöbbsorba(munkalap,"B8", true);
+
+            MyX.Kiir("E1", "b9");
+            MyX.Kiir("E2", "c9");
+            MyX.Kiir("E3", "d9");
+            MyX.Egyesít(munkalap, "e8:f8");
+            MyX.Kiir("Takarítás ideje", "e8");
+            MyX.Kiir("-tól", "e9");
+            MyX.Kiir("-ig", "f9");
+            MyX.Egyesít(munkalap, "g8:g9");
+            MyX.Kiir("Aláírás", "g8");
+            MyX.Egyesít(munkalap, "h8:i8");
+            MyX.Kiir("Megfelelő", "h8");
+            MyX.Kiir("I", "h9");
+            MyX.Kiir("N", "i9");
+            MyX.Egyesít(munkalap, "j8:j9");
+            MyX.Kiir("Igazolta", "j8");
+            MyX.Egyesít(munkalap, "k8:k9");
+            MyX.Kiir("Pót. Határ- idő", "k8");
+            MyX.Sortörésseltöbbsorba(munkalap, "K8", true);
+            MyX.Egyesít(munkalap, "l8:m8");
+            MyX.Kiir("Megfelelő", "l8");
+            MyX.Kiir("I", "l9");
+            MyX.Kiir("N", "m9");
+            MyX.Egyesít(munkalap, "n8:n9");
+            MyX.Kiir("Igazolta", "n8");
+            MyX.Rácsoz(munkalap,"a7:n9");
+            MyX.Vastagkeret(munkalap,"a8");
+            MyX.Vastagkeret(munkalap,"b8:g9");
+            MyX.Vastagkeret(munkalap,"h8:n9");
+            int sor = 1;
+
+            Adat_Épület_Takarításrakijelölt rekord = (from a in AdatokKijelöltek
+                                                      where a.Hónap == Dátum.Month
+                                                      && a.Helységkód == helységkód.Trim()
+                                                      select a).FirstOrDefault();
+
+            if (rekord != null)
+            {
+                // kiirjuk a helység nevét
+                string szöveg1 = rekord.Helységkód.Trim() + " - " + rekord.Megnevezés.Trim();
+
+                List<Adat_Épület_Adattábla> AdatokÉ = (from a in AdatokAdatTábla
+                                                       where a.Státus == false
+                                                       && a.Kapcsolthelység == helységkód.Trim()
+                                                       select a).ToList();
+
+                if (AdatokÉ != null)
+                {
+                    foreach (Adat_Épület_Adattábla rekordép in AdatokÉ)
+                        szöveg1 += "; " + rekordép.Helységkód.Trim() + " - " + rekordép.Megnevezés.Trim();
+                }
+
+                MyX.Kiir(szöveg1, "a2");
+                MyX.Sortörésseltöbbsorba(munkalap,"a2", true);
+                MyX.Igazít_vízszintes(munkalap,"a2", "közép");
+
+                sor = 10;
+
+                for (int i = 0; i < hónapnap; i++)
+                {
+                    if (MyF.Szöveg_Tisztítás(rekord.E1rekijelölt, i, 1) == "0")
+                        MyX.Háttérszín(munkalap,"b" + sor.ToString(), Color.FromArgb(12632256));
+                    if (MyF.Szöveg_Tisztítás(rekord.E2rekijelölt, i, 1) == "0")
+                        MyX.Háttérszín(munkalap,"c" + sor.ToString(), Color.FromArgb(12632256));
+                    if (MyF.Szöveg_Tisztítás(rekord.E3rekijelölt, i, 1) == "0")
+                        MyX.Háttérszín(munkalap,"d" + sor.ToString(), Color.FromArgb(12632256));
+                    sor += 1;
+                }
+            }
+
+            sor = 10;
+
+            for (int i = 0; i < hónapnap; i++)
+            {
+                MyX.Kiir((i + 1).ToString(), "a" + sor.ToString());
+                sor += 1;
+            }
+            MyX.Kiir("Össz", "a" + sor.ToString());
+            MyX.Betű(munkalap,"a" + sor.ToString(), BeBetűVastag);
+            MyX.Rácsoz(munkalap,"a10:n" + sor.ToString());
+            MyX.Vastagkeret(munkalap,"b10:g" + sor.ToString());
+            MyX.Rácsoz(munkalap, $"B10:G{sor}");
+            MyX.Vastagkeret(munkalap,"h10:n" + sor.ToString());
+            MyX.Rácsoz(munkalap, $"H10:N{sor}");
+            MyX.Vastagkeret(munkalap,"a" + sor.ToString() + ":n" + sor.ToString());
+            MyX.Rácsoz(munkalap, $"A{sor}:N{sor}");
+            // Szombat vasárnap
+            Adat_Épület_Naptár Naptár = (from a in AdatokÉNaptár
+                                         where a.Hónap == Dátum.Month
+                                         select a).FirstOrDefault();
+
+            if (Naptár != null)
+            {
+                sor = 10;
+                
+                for (int i = 0; i < hónapnap; i++)
+                {                    
+                    if (MyF.Szöveg_Tisztítás(Naptár.Napok, i, 1) == "0")
+                    {
+                        // ferde vonal
+                        Beállítás_Ferde BeállítFerde = new Beállítás_Ferde() { Munkalap = munkalap, Terület = $"B{sor}:N{sor}" };
+                        MyX.FerdeVonal(BeállítFerde);
+                    }
+                    sor += 1;
+                }
+            }
+
+            sor += 2;
+            // jelmagyarázat
+            MyX.Kiir("Jelmagyarázat", "a" + sor.ToString());
+            sor += 1;
+            MyX.Vékonykeret(munkalap,"a" + sor.ToString());
+            MyX.Kiir("Megrendelt takarítás", "b" + sor.ToString());
+            sor += 1;
+            MyX.Vékonykeret(munkalap,"a" + sor.ToString());
+            MyX.Háttérszín(munkalap,"a" + sor.ToString(), Color.FromArgb(12632256));
+            MyX.Kiir("Nincs megrendelve a takarítás", "b" + sor.ToString());
+            sor += 1;
+            MyX.Vékonykeret(munkalap,"a" + sor.ToString());
+            Beállítás_Ferde BeállítFerde_ = new Beállítás_Ferde() { Munkalap = munkalap, Terület = $"A{sor}" };
+            MyX.FerdeVonal(BeállítFerde_);
+            MyX.Háttérszín(munkalap,"a" + sor.ToString(), Color.FromArgb(12632256));
+            MyX.Kiir("Munkaszüneti nap", "b" + sor.ToString());
+
+            // **********************************************
+            // **Nyomtatási beállítások                    **
+            // **********************************************
+            Beállítás_Nyomtatás NyomtatásBeállít = new Beállítás_Nyomtatás() { Munkalap = munkalap, NyomtatásiTerület = "a1:n" + sor, BalMargó = 10, JobbMargó = 10, AlsóMargó = 15, FelsőMargó = 15, FejlécMéret = 13, LáblécMéret = 13, LapSzéles = 1, LapMagas = 1 };
+            MyX.NyomtatásiTerület_részletes(munkalap, NyomtatásBeállít);
+
+            // **********************************************
+            // **Nyomtatás                                 **
+            // **********************************************
+            //if (Option9.Checked) MyX.Nyomtatás(munkalap, 1, 1);
+
+            // bezárjuk az Excel-t
+            MyX.Aktív_Cella(munkalap, "A1");
+            MyX.ExcelMentés(fájlexc);
+            MyX.ExcelBezárás();
+        }
+    }
+}
