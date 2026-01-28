155ce73ead911ae477e8e39421694c8370fc9df9
clanishcoffee <richard_pap@stud.uni-obuda.hu>
2026-01-28 09:32:35 +0100

    SQLite bevezető fájlok

---
+++ V_Adatszerkezet/Adat_SQLite.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamos.V_Adatszerkezet
+{
+    public class Adat_SQLite
+    {
+        public Adat_SQLite(string username, DateTime date, int age, bool trueOrFalse)
+        {
+            Username = username;
+            Date = date;
+            Age = age;
+            TrueOrFalse = trueOrFalse;
+        }
+
+        public string Username { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int Age { get; set; }
+
+        public bool TrueOrFalse { get; set; }
+
+    }
+}
