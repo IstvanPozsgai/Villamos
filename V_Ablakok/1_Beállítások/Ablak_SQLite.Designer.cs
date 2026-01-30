b0e5de86be5db79f1eb199fbdec54c07495d02aa
clanishcoffee <richard_pap@stud.uni-obuda.hu>
2026-01-30 06:34:03 +0100

    Revert "SQLite CRUD műveletek"
    This reverts commit 6c76a908d776fb051b4e3b2aa676bf49cdf06180.

--- V_Ablakok/1_Beállítások/Ablak_SQLite.Designer.cs
+++ V_Ablakok/1_Beállítások/Ablak_SQLite.Designer.cs
@@ -40,8 +40,6 @@
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.btn_create = new System.Windows.Forms.Button();
             this.btn_AddData = new System.Windows.Forms.Button();
-            this.btn_Update = new System.Windows.Forms.Button();
-            this.btn_Delete = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_ShowData)).BeginInit();
             this.tblP_ShowUserData.SuspendLayout();
@@ -60,8 +58,8 @@
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 29.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70.66666F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
             this.tableLayoutPanel1.TabIndex = 0;
@@ -73,11 +71,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgv_ShowData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.tableLayoutPanel1.SetColumnSpan(this.dgv_ShowData, 2);
-            this.dgv_ShowData.Location = new System.Drawing.Point(3, 135);
+            this.dgv_ShowData.Location = new System.Drawing.Point(3, 228);
             this.dgv_ShowData.Name = "dgv_ShowData";
-            this.dgv_ShowData.Size = new System.Drawing.Size(794, 312);
+            this.dgv_ShowData.Size = new System.Drawing.Size(794, 219);
             this.dgv_ShowData.TabIndex = 0;
-            this.dgv_ShowData.SelectionChanged += new System.EventHandler(this.dgv_ShowData_SelectionChanged);
             // 
             // tblP_ShowUserData
             // 
@@ -97,7 +94,7 @@
             this.tblP_ShowUserData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tblP_ShowUserData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tblP_ShowUserData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 76F));
-            this.tblP_ShowUserData.Size = new System.Drawing.Size(558, 126);
+            this.tblP_ShowUserData.Size = new System.Drawing.Size(558, 219);
             this.tblP_ShowUserData.TabIndex = 2;
             // 
             // lb_username
@@ -106,14 +103,14 @@
             this.lb_username.Dock = System.Windows.Forms.DockStyle.Left;
             this.lb_username.Location = new System.Drawing.Point(3, 0);
             this.lb_username.Name = "lb_username";
-            this.lb_username.Size = new System.Drawing.Size(27, 25);
+            this.lb_username.Size = new System.Drawing.Size(27, 71);
             this.lb_username.TabIndex = 0;
             this.lb_username.Text = "Név";
             // 
             // lb_datetime
             // 
             this.lb_datetime.AutoSize = true;
-            this.lb_datetime.Location = new System.Drawing.Point(3, 25);
+            this.lb_datetime.Location = new System.Drawing.Point(3, 71);
             this.lb_datetime.Name = "lb_datetime";
             this.lb_datetime.Size = new System.Drawing.Size(38, 13);
             this.lb_datetime.TabIndex = 1;
@@ -122,7 +119,7 @@
             // lb_tf
             // 
             this.lb_tf.AutoSize = true;
-            this.lb_tf.Location = new System.Drawing.Point(3, 50);
+            this.lb_tf.Location = new System.Drawing.Point(3, 142);
             this.lb_tf.Name = "lb_tf";
             this.lb_tf.Size = new System.Drawing.Size(33, 13);
             this.lb_tf.TabIndex = 2;
@@ -138,7 +135,7 @@
             // 
             // tb_datetime
             // 
-            this.tb_datetime.Location = new System.Drawing.Point(159, 28);
+            this.tb_datetime.Location = new System.Drawing.Point(159, 74);
             this.tb_datetime.Name = "tb_datetime";
             this.tb_datetime.ReadOnly = true;
             this.tb_datetime.Size = new System.Drawing.Size(100, 20);
@@ -146,7 +143,7 @@
             // 
             // tb_tf
             // 
-            this.tb_tf.Location = new System.Drawing.Point(159, 53);
+            this.tb_tf.Location = new System.Drawing.Point(159, 145);
             this.tb_tf.Name = "tb_tf";
             this.tb_tf.ReadOnly = true;
             this.tb_tf.Size = new System.Drawing.Size(100, 20);
@@ -154,67 +151,39 @@
             // 
             // tableLayoutPanel2
             // 
-            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnCount = 1;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 123F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.Controls.Add(this.btn_create, 0, 1);
             this.tableLayoutPanel2.Controls.Add(this.btn_AddData, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.btn_Update, 1, 0);
-            this.tableLayoutPanel2.Controls.Add(this.btn_Delete, 1, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(567, 3);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 3;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.54237F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.45763F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(230, 126);
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.24201F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 86.75799F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(230, 219);
             this.tableLayoutPanel2.TabIndex = 3;
             // 
             // btn_create
             // 
-            this.btn_create.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn_create.Location = new System.Drawing.Point(3, 34);
+            this.btn_create.Location = new System.Drawing.Point(3, 32);
             this.btn_create.Name = "btn_create";
-            this.btn_create.Size = new System.Drawing.Size(101, 22);
+            this.btn_create.Size = new System.Drawing.Size(75, 23);
             this.btn_create.TabIndex = 3;
-            this.btn_create.Text = "ADB Létrehoz";
+            this.btn_create.Text = "Létrehoz";
             this.btn_create.UseVisualStyleBackColor = true;
             this.btn_create.Click += new System.EventHandler(this.btn_create_Click);
             // 
             // btn_AddData
             // 
-            this.btn_AddData.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btn_AddData.Location = new System.Drawing.Point(3, 3);
             this.btn_AddData.Name = "btn_AddData";
-            this.btn_AddData.Size = new System.Drawing.Size(101, 25);
+            this.btn_AddData.Size = new System.Drawing.Size(75, 23);
             this.btn_AddData.TabIndex = 2;
             this.btn_AddData.Text = "Rögzít";
             this.btn_AddData.UseVisualStyleBackColor = true;
             this.btn_AddData.Click += new System.EventHandler(this.btn_AddData_Click);
-            // 
-            // btn_Update
-            // 
-            this.btn_Update.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn_Update.Location = new System.Drawing.Point(110, 3);
-            this.btn_Update.Name = "btn_Update";
-            this.btn_Update.Size = new System.Drawing.Size(117, 25);
-            this.btn_Update.TabIndex = 4;
-            this.btn_Update.Text = "Frissít";
-            this.btn_Update.UseVisualStyleBackColor = true;
-            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
-            // 
-            // btn_Delete
-            // 
-            this.btn_Delete.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn_Delete.Location = new System.Drawing.Point(110, 34);
-            this.btn_Delete.Name = "btn_Delete";
-            this.btn_Delete.Size = new System.Drawing.Size(117, 22);
-            this.btn_Delete.TabIndex = 5;
-            this.btn_Delete.Text = "Töröl";
-            this.btn_Delete.UseVisualStyleBackColor = true;
-            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
             // 
             // Ablak_SQLite
             // 
@@ -223,7 +192,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Ablak_SQLite";
-            this.Text = "SQLite teszt";
+            this.Text = "Ablak_SQLite";
             this.Load += new System.EventHandler(this.Ablak_SQLite_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgv_ShowData)).EndInit();
@@ -248,7 +217,5 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.Button btn_create;
         private System.Windows.Forms.Button btn_AddData;
-        private System.Windows.Forms.Button btn_Update;
-        private System.Windows.Forms.Button btn_Delete;
     }
 }
--- V_Ablakok/1_Beállítások/Ablak_SQLite.cs
+++ V_Ablakok/1_Beállítások/Ablak_SQLite.cs
@@ -7,69 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Villamos.Adatszerkezet;
 using Villamos.Kezelők;
-using Villamos.V_Adatszerkezet;
 
 namespace Villamos.V_Ablakok._1_Beállítások
 {
     public partial class Ablak_SQLite : Form
     {
         Kezelő_SQLite Kezelő_SQL = new Kezelő_SQLite();
-        int _selectedId;
-        bool _formLoaded;
-        string username = null;   
-
-        public Ablak_SQLite(string Username)
+        public Ablak_SQLite()
         {
             InitializeComponent();
-            username = Username;
         }
+
+        private void btn_AddData_Click(object sender, EventArgs e)
+        {            
+            Kezelő_SQL.InsertData(tb_username.Text, DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToÉrt_Int(), tb_tf.Text.ToÉrt_Int());
+        }
+
         private void Ablak_SQLite_Load(object sender, EventArgs e)
         {
-            tb_username.Text = username;
-            tb_datetime.Text = DateTime.Now.ToString();
+            tb_username.Text = "Teszt Jóska";
+            tb_datetime.Text = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
             tb_tf.Text = "1";
+        }
 
-            FillDGV();
-            _formLoaded = true;
-        }
-        private void btn_AddData_Click(object sender, EventArgs e)
-        {
-            Kezelő_SQL.InsertData(username, DateTimeOffset.Now.ToUnixTimeSeconds().ToÉrt_Int(), tb_tf.Text.ToÉrt_Int());
-            FillDGV();
-        }
         private void btn_create_Click(object sender, EventArgs e)
         {
             Kezelő_SQL.CreateTable();
-        }
-        private void btn_Update_Click(object sender, EventArgs e)
-        {
-            Kezelő_SQL.UpdateData(Kezelő_SQL.ReadAllData().FirstOrDefault(x => x.ID == _selectedId), _selectedId);
-            FillDGV();
-        }
-        private void btn_Delete_Click(object sender, EventArgs e)
-        {
-            Kezelő_SQL.DeleteData(_selectedId);
-            FillDGV();
-        }
-        private void dgv_ShowData_SelectionChanged(object sender, EventArgs e)
-        {
-            if (!_formLoaded)
-                return;
-
-            if (dgv_ShowData.CurrentRow == null)
-                return;
-
-            if (dgv_ShowData.CurrentRow.DataBoundItem is Adat_SQLite adat)
-            {
-                _selectedId = adat.ID;               
-            }
-        }
-        private void FillDGV()
-        {
-            dgv_ShowData.AutoGenerateColumns = true;
-            dgv_ShowData.DataSource = Kezelő_SQL.ReadAllData();
         }
     }
 }
