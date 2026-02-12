294c13875d3a03f184138b07af83470d07ad6b85
Tundi21 <gallotundi@gmail.com>
2026-02-12 10:58:58 +0100

    Jármű2 osztályainak szétszedése

--- V_Adatszerkezet/Adat_Jármű_2.cs
+++ V_Adatszerkezet/Adat_Jármű_2.cs
@@ -23,19 +23,4 @@ namespace Villamos.Adatszerkezet
         }
     }
 
-    public class Adat_Jármű_2ICS
-    {
-        public string Azonosító { get; private set; }
-        public DateTime Takarítás { get; private set; }
-        public int E2 { get; private set; }
-        public int E3 { get; private set; }
-
-        public Adat_Jármű_2ICS(string azonosító, DateTime takarítás, int e2, int e3)
-        {
-            Azonosító = azonosító;
-            Takarítás = takarítás;
-            E2 = e2;
-            E3 = e3;
-        }
-    }
 }
---
+++ V_Adatszerkezet/Adat_Jármű_2ICS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamos.Adatszerkezet
+{
+    public class Adat_Jármű_2ICS
+    {
+        public string Azonosító { get; private set; }
+        public DateTime Takarítás { get; private set; }
+        public int E2 { get; private set; }
+        public int E3 { get; private set; }
+
+        public Adat_Jármű_2ICS(string azonosító, DateTime takarítás, int e2, int e3)
+        {
+            Azonosító = azonosító;
+            Takarítás = takarítás;
+            E2 = e2;
+            E3 = e3;
+        }
+    }
+}
