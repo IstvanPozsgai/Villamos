1d8926d3192424f3f2ca3e69c4c905f50bf1eacf
Tundi21 <gallotundi@gmail.com>
2026-02-12 13:24:15 +0100

    Osztály adat osztályainak szétszedése

--- V_Adatszerkezet/Adat_Osztály_Adat.cs
+++ V_Adatszerkezet/Adat_Osztály_Adat.cs
@@ -26,20 +26,4 @@ namespace Villamos.Adatszerkezet
             Típus = típus;
         }
     }
-
-    public class Adat_Osztály_Név
-    {
-        public int Id { get; private set; }
-        public string Osztálynév { get; private set; }
-        public string Osztálymező { get; private set; }
-        public bool Használatban { get; private set; }
-
-        public Adat_Osztály_Név(int id, string osztálynév, string osztálymező, bool használatban)
-        {
-            Id = id;
-            Osztálynév = osztálynév;
-            Osztálymező = osztálymező;
-            Használatban = használatban;
-        }
-    }
 }
---
+++ V_Adatszerkezet/Adat_Osztály_Név.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamos.Adatszerkezet
+{
+    public class Adat_Osztály_Név
+    {
+        public int Id { get; private set; }
+        public string Osztálynév { get; private set; }
+        public string Osztálymező { get; private set; }
+        public bool Használatban { get; private set; }
+
+        public Adat_Osztály_Név(int id, string osztálynév, string osztálymező, bool használatban)
+        {
+            Id = id;
+            Osztálynév = osztálynév;
+            Osztálymező = osztálymező;
+            Használatban = használatban;
+        }
+    }
+}
