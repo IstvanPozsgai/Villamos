0f1625317ea6b173a1c42f2cbc27d9386d034e58
Tundi21 <gallotundi@gmail.com>
2026-02-12 13:42:14 +0100

    Takarítás opció osztályai

--- V_Adatszerkezet/Adat_Takarítás_Opció.cs
+++ V_Adatszerkezet/Adat_Takarítás_Opció.cs
@@ -28,19 +28,4 @@ namespace Villamos.Adatszerkezet
         }
     }
 
-    public class Adat_Takarítás_Telep_Opció
-    {
-        public int Id { get; private set; }
-        public DateTime Dátum { get; private set; }
-        public double Megrendelt { get; private set; }
-        public double Teljesített { get; private set; }
-
-        public Adat_Takarítás_Telep_Opció(int id, DateTime dátum, double megrendelt, double teljesített)
-        {
-            Id = id;
-            Dátum = dátum;
-            Megrendelt = megrendelt;
-            Teljesített = teljesített;
-        }
-    }
 }
---
+++ V_Adatszerkezet/Adat_Takarítás_Telep_Opció.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamos.Adatszerkezet
+{
+    public class Adat_Takarítás_Telep_Opció
+    {
+        public int Id { get; private set; }
+        public DateTime Dátum { get; private set; }
+        public double Megrendelt { get; private set; }
+        public double Teljesített { get; private set; }
+
+        public Adat_Takarítás_Telep_Opció(int id, DateTime dátum, double megrendelt, double teljesített)
+        {
+            Id = id;
+            Dátum = dátum;
+            Megrendelt = megrendelt;
+            Teljesített = teljesített;
+        }
+    }
+}
