fb670b32dd877d7b8e0ebd9590920c1021872c2a
Tundi21 <gallotundi@gmail.com>
2026-02-12 11:51:51 +0100

    Kerékmérő osztályainak szétszedése

--- V_Adatszerkezet/Adat_KerékMérő.cs
+++ V_Adatszerkezet/Adat_KerékMérő.cs
@@ -105,24 +105,4 @@ namespace Villamos.Villamos_Ablakok.MEO
         }
     }
 
-
-    public class Adat_KerékmérőTengely
-    {
-        public string Név { get; private set; }
-
-        public string SAP { get; private set; }
-
-        public long Km { get; private set; }
-
-        public Adat_KerékmérőTengely(string név, string sAP)
-        {
-            Név = név;
-            SAP = sAP;
-        }
-
-        public Adat_KerékmérőTengely(string név, string sAP, long km) : this(név, sAP)
-        {
-            Km = km;
-        }
-    }
 }
---
+++ V_Adatszerkezet/Adat_KerékmérőTengely.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamos.Adatszerkezet
+{
+    public class Adat_KerékmérőTengely
+    {
+        public string Név { get; private set; }
+
+        public string SAP { get; private set; }
+
+        public long Km { get; private set; }
+
+        public Adat_KerékmérőTengely(string név, string sAP)
+        {
+            Név = név;
+            SAP = sAP;
+        }
+
+        public Adat_KerékmérőTengely(string név, string sAP, long km) : this(név, sAP)
+        {
+            Km = km;
+        }
+    }
+}
