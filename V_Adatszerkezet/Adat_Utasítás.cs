587fd53a7447d2890ffeeab715b0c9855b47472a
Tundi21 <gallotundi@gmail.com>
2026-02-12 13:49:35 +0100

    Utasítás osztályai

--- V_Adatszerkezet/Adat_Utasítás.cs
+++ V_Adatszerkezet/Adat_Utasítás.cs
@@ -27,25 +27,7 @@ namespace Villamos.Adatszerkezet
             Érvényes = érvényes;
         }
     }
-    public class Adat_utasítás_olvasás
-    {
-        public double Sorszám { get; private set; }
-        public string Ki { get; private set; }
-        public double Üzenetid { get; private set; }
-        public DateTime Mikor { get; private set; }
-        public bool Olvasva { get; private set; }
 
-        public Adat_utasítás_olvasás(double sorszám, string ki, double üzenetid, DateTime mikor, bool olvasva)
-        {
-            Sorszám = sorszám;
-            Ki = ki;
-            Üzenetid = üzenetid;
-            Mikor = mikor;
-            Olvasva = olvasva;
-        }
-
-
-    }
 
 
 }
---
+++ V_Adatszerkezet/Adat_utasítás_olvasás.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamos.Adatszerkezet
+{
+    public class Adat_utasítás_olvasás
+    {
+        public double Sorszám { get; private set; }
+        public string Ki { get; private set; }
+        public double Üzenetid { get; private set; }
+        public DateTime Mikor { get; private set; }
+        public bool Olvasva { get; private set; }
+
+        public Adat_utasítás_olvasás(double sorszám, string ki, double üzenetid, DateTime mikor, bool olvasva)
+        {
+            Sorszám = sorszám;
+            Ki = ki;
+            Üzenetid = üzenetid;
+            Mikor = mikor;
+            Olvasva = olvasva;
+        }
+
+
+    }
+}
