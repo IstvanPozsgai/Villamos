ca26cd012825623ad8c390c62357bc6cff9ca878
Tundi21 <gallotundi@gmail.com>
2026-02-12 13:50:33 +0100

    Üzenet osztályai

--- V_Adatszerkezet/Adat_Üzenet.cs
+++ V_Adatszerkezet/Adat_Üzenet.cs
@@ -24,30 +24,5 @@ namespace Villamos.Adatszerkezet
         }
     }
 
-    public class Adat_Üzenet_Olvasás
-    {
-        public double Sorszám { get; private set; }
-        public string Ki { get; private set; }
-        public double Üzenetid { get; private set; }
-        public DateTime Mikor { get; private set; }
-        public bool Olvasva { get; private set; }
-
-        public Adat_Üzenet_Olvasás(double sorszám, string ki, double üzenetid, DateTime mikor, bool olvasva)
-        {
-            Sorszám = sorszám;
-            Ki = ki;
-            Üzenetid = üzenetid;
-            Mikor = mikor;
-            Olvasva = olvasva;
-        }
-
-        public Adat_Üzenet_Olvasás(double sorszám, string ki, double üzenetid, DateTime mikor)
-        {
-            Sorszám = sorszám;
-            Ki = ki;
-            Üzenetid = üzenetid;
-            Mikor = mikor;
-        }
-    }
 
 }
---
+++ V_Adatszerkezet/Adat_Üzenet_Olvasás.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamos.Adatszerkezet
+{
+    public class Adat_Üzenet_Olvasás
+    {
+        public double Sorszám { get; private set; }
+        public string Ki { get; private set; }
+        public double Üzenetid { get; private set; }
+        public DateTime Mikor { get; private set; }
+        public bool Olvasva { get; private set; }
+
+        public Adat_Üzenet_Olvasás(double sorszám, string ki, double üzenetid, DateTime mikor, bool olvasva)
+        {
+            Sorszám = sorszám;
+            Ki = ki;
+            Üzenetid = üzenetid;
+            Mikor = mikor;
+            Olvasva = olvasva;
+        }
+
+        public Adat_Üzenet_Olvasás(double sorszám, string ki, double üzenetid, DateTime mikor)
+        {
+            Sorszám = sorszám;
+            Ki = ki;
+            Üzenetid = üzenetid;
+            Mikor = mikor;
+        }
+    }
+}
