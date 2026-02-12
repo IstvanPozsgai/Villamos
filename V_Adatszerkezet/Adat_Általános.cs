9c6d156ee70881d3d9d5a0c0f0ca01f014f34d09
Tundi21 <gallotundi@gmail.com>
2026-02-12 09:07:09 +0100

    Nem használt konstruktorok törlése

--- V_Adatszerkezet/Adat_Általános.cs
+++ V_Adatszerkezet/Adat_Általános.cs
@@ -6,30 +6,6 @@ using System.Threading.Tasks;
 
 namespace Villamos.Adatszerkezet
 {
-    public  class Adat_Általános_Long_String
-    {
-        public long Id { get;private set; }
-        public string  Szöveg { get; private set; }
-
-        public Adat_Általános_Long_String(long id, string szöveg)
-        {
-            Id = id;
-            Szöveg = szöveg;
-        }
-    }
-
-    public class Adat_Általános_Int_String
-    {
-        public int Id { get; private set; }
-        public string Szöveg { get; private set; }
-
-        public Adat_Általános_Int_String(int id, string szöveg)
-        {
-            Id = id;
-            Szöveg = szöveg;
-        }
-    }
-
     public class Adat_Általános_String_Dátum
     {
         public DateTime  Dátum { get; private set; }
@@ -41,20 +17,5 @@ namespace Villamos.Adatszerkezet
             Szöveg = szöveg;
         }
     }
-
-    public class Szín
-    {
-        public int Red { get; private set; }
-        public int Green { get; private set; }
-        public int Blue { get; private set; }
-
-        public Szín(int red, int green, int blue)
-        {
-            Red = red;
-            Green = green;
-            Blue = blue;
-        }
-    }
-
 
 }
