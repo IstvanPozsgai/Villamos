5a817325ebe3a64634d4a5356cbb26f9edb930fc
Tundi21 <gallotundi@gmail.com>
2026-02-12 10:14:19 +0100

    Digitális munkalap osztályainak szétszedése

---
+++ V_Adatszerkezet/Adat_DigitálisMunkalap_Dolgozó.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamos.Adatszerkezet
+{
+    public class Adat_DigitálisMunkalap_Dolgozó
+    {
+        public string DolgozóNév { get; private set; }
+        public string Dolgozószám { get; private set; }
+
+        public long Fej_Id { get; private set; }
+        public long Technológia_Id { get; private set; }
+
+        public Adat_DigitálisMunkalap_Dolgozó(string dolgozóNév, string dolgozószám, long fej_Id, long technológia_Id)
+        {
+            DolgozóNév = dolgozóNév;
+            Dolgozószám = dolgozószám;
+            Fej_Id = fej_Id;
+            Technológia_Id = technológia_Id;
+        }
+    }
+}
---
+++ V_Adatszerkezet/Adat_DigitálisMunkalap_Kocsik.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamos.Adatszerkezet
+{
+    public class Adat_DigitálisMunkalap_Kocsik
+    {
+        public long Fej_Id { get; private set; }
+
+        public string Azonosító { get; private set; }
+
+        public long KMU { get; private set; }
+        public string Rendelés { get; private set; }
+
+        public Adat_DigitálisMunkalap_Kocsik(long fej_Id, string azonosító, long kMU, string rendelés)
+        {
+            Fej_Id = fej_Id;
+            Azonosító = azonosító;
+            KMU = kMU;
+            Rendelés = rendelés;
+        }
+    }
+}
