f944080794a3111456cbf673ea63203544873198
Tundi21 <gallotundi@gmail.com>
2026-02-12 13:18:36 +0100

    Nóta osztályainak szétszedése

--- V_Adatszerkezet/Adat_Nóta.cs
+++ V_Adatszerkezet/Adat_Nóta.cs
@@ -44,23 +44,4 @@ namespace Villamos.Adatszerkezet
         }
     }
 
-    public class Adat_Nóta_SAP
-    {
-        public string Berendezés { get; private set; }
-        public string Rendszerstátus { get; private set; }
-        public string Készlet_Sarzs { get; private set; }
-        public string Raktár { get; private set; }
-        public string Rendezési { get; private set; }
-        public string Cikkszám { get; private set; }
-
-        public Adat_Nóta_SAP(string berendezés, string rendszerstátus, string készlet_Sarzs, string raktár, string rendezési, string cikkszám)
-        {
-            Berendezés = berendezés;
-            Rendszerstátus = rendszerstátus;
-            Készlet_Sarzs = készlet_Sarzs;
-            Raktár = raktár;
-            Rendezési = rendezési;
-            Cikkszám = cikkszám;
-        }
-    }
 }
---
+++ V_Adatszerkezet/Adat_Nóta_SAP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamos.Adatszerkezet
+{
+    public class Adat_Nóta_SAP
+    {
+        public string Berendezés { get; private set; }
+        public string Rendszerstátus { get; private set; }
+        public string Készlet_Sarzs { get; private set; }
+        public string Raktár { get; private set; }
+        public string Rendezési { get; private set; }
+        public string Cikkszám { get; private set; }
+
+        public Adat_Nóta_SAP(string berendezés, string rendszerstátus, string készlet_Sarzs, string raktár, string rendezési, string cikkszám)
+        {
+            Berendezés = berendezés;
+            Rendszerstátus = rendszerstátus;
+            Készlet_Sarzs = készlet_Sarzs;
+            Raktár = raktár;
+            Rendezési = rendezési;
+            Cikkszám = cikkszám;
+        }
+    }
+}
