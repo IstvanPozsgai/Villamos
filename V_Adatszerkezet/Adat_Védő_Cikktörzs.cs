bfafc6071964d92bba8e0a5ae27ddd047777c785
Tundi21 <gallotundi@gmail.com>
2026-02-12 13:54:02 +0100

    Védő osztályai

---
+++ V_Adatszerkezet/Adat_Védő_Cikktörzs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamos.Adatszerkezet
+{
+    public class Adat_Védő_Cikktörzs
+    {
+        public string Azonosító { get; private set; }
+        public string Megnevezés { get; private set; }
+        public string Méret { get; private set; }
+        public int Státus { get; private set; }
+
+        public string Költséghely { get; private set; }
+        public string Védelem { get; private set; }
+        public string Kockázat { get; private set; }
+
+        public string Szabvány { get; private set; }
+        public string Szint { get; private set; }
+        public string Munk_megnevezés { get; private set; }
+
+        public Adat_Védő_Cikktörzs(string azonosító, string megnevezés, string méret, int státus, string költséghely, string védelem, string kockázat, string szabvány, string szint, string munk_megnevezés)
+        {
+            Azonosító = azonosító;
+            Megnevezés = megnevezés;
+            Méret = méret;
+            Státus = státus;
+            Költséghely = költséghely;
+            Védelem = védelem;
+            Kockázat = kockázat;
+            Szabvány = szabvány;
+            Szint = szint;
+            Munk_megnevezés = munk_megnevezés;
+        }
+    }
+}
---
+++ V_Adatszerkezet/Adat_Védő_Könyvelés.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Villamos.Adatszerkezet
+{
+    public class Adat_Védő_Könyvelés
+    {
+        public string Azonosító { get; private set; }
+        public string Szerszámkönyvszám { get; private set; }
+        public double Mennyiség { get; private set; }
+
+        public string Gyáriszám { get; private set; }
+        public DateTime Dátum { get; private set; }
+        public bool Státus { get; private set; }
+
+        public Adat_Védő_Könyvelés(string azonosító, string szerszámkönyvszám, double mennyiség, string gyáriszám, DateTime dátum, bool státus)
+        {
+            Azonosító = azonosító;
+            Szerszámkönyvszám = szerszámkönyvszám;
+            Mennyiség = mennyiség;
+            Gyáriszám = gyáriszám;
+            Dátum = dátum;
+            Státus = státus;
+        }
+    }
+}
