514eafc3f8036777b9c11c2c4fc6a18e420156a4
Pozsgai István <117170572+IstvanPozsgai@users.noreply.github.com>
2026-02-18 17:31:03 +0100

    verzió

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("26.02.18.2")]
-[assembly: AssemblyFileVersion("26.02.18.2")]
+[assembly: AssemblyVersion("26.02.18.3")]
+[assembly: AssemblyFileVersion("26.02.18.3")]
 [assembly: NeutralResourcesLanguage("hu-HU")]
