3827788a578660277396a0abf6a695490fdef384
Pozsgai István <117170572+IstvanPozsgai@users.noreply.github.com>
2026-03-22 19:43:56 +0100

    SqLite alapelv kidolgozás

---
+++ SqL_Kezelők/Rezsi/SQL_Kezelő_Rezsi_Könyvelés.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.IO;
+using System.Windows.Forms;
+using Villamos.Adatszerkezet;
+using Villamos.Villamos_Adatbázis_Funkció;
+using MyA = Adatbázis;
+
+namespace Villamos.Kezelők
+{
+    public class SQL_Kezelő_Rezsi_Könyvelés
+    {
+        readonly string jelszó = "csavarhúzó";
+        readonly string Táblanév = "könyv";
+        string hely;
+
+        private void FájlBeállítás(string Telephely)
+        {
+            hely = $@"{Application.StartupPath}\{Telephely}\Adatok\Rezsi\rezsikönyv.mdb";
+            if (!File.Exists(hely)) Adatbázis_Létrehozás.Rezsilista(hely);
+        }
+
+        public List<Adat_Rezsi_Lista> Lista_Adatok(string Telephely)
+        {
+            FájlBeállítás(Telephely);
+            string szöveg = $"SELECT * FROM {Táblanév} ORDER BY Azonosító";
+            List<Adat_Rezsi_Lista> Adatok = new List<Adat_Rezsi_Lista>();
+            Adat_Rezsi_Lista Adat;
+
+            string kapcsolatiszöveg = $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source='{hely}'; Jet Oledb:Database Password={jelszó}";
+            using (OleDbConnection Kapcsolat = new OleDbConnection(kapcsolatiszöveg))
+            {
+                Kapcsolat.Open();
+                using (OleDbCommand Parancs = new OleDbCommand(szöveg, Kapcsolat))
+                {
+                    using (OleDbDataReader rekord = Parancs.ExecuteReader())
+                    {
+                        if (rekord.HasRows)
+                        {
+                            while (rekord.Read())
+                            {
+                                Adat = new Adat_Rezsi_Lista(
+                                       rekord["Azonosító"].ToStrTrim(),
+                                       rekord["Mennyiség"].ToÉrt_Double(),
+                                       rekord["Dátum"].ToÉrt_DaTeTime(),
+                                       rekord["státus"].ToÉrt_Bool());
+                                Adatok.Add(Adat);
+                            }
+                        }
+                    }
+                }
+            }
+            return Adatok;
+        }
+
+        public List<Adat_Rezsi_Lista> Lista_Adatok(List<string> Telephely)
+        {
+            List<Adat_Rezsi_Lista> Adatok = new List<Adat_Rezsi_Lista>();
+            foreach (string telep in Telephely)
+            {
+                FájlBeállítás(telep);
+                string szöveg = $"SELECT * FROM {Táblanév} ORDER BY Azonosító";
+                Adat_Rezsi_Lista Adat;
+
+                string kapcsolatiszöveg = $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source='{hely}'; Jet Oledb:Database Password={jelszó}";
+                using (OleDbConnection Kapcsolat = new OleDbConnection(kapcsolatiszöveg))
+                {
+                    Kapcsolat.Open();
+                    using (OleDbCommand Parancs = new OleDbCommand(szöveg, Kapcsolat))
+                    {
+                        using (OleDbDataReader rekord = Parancs.ExecuteReader())
+                        {
+                            if (rekord.HasRows)
+                            {
+                                while (rekord.Read())
+                                {
+                                    Adat = new Adat_Rezsi_Lista(
+                                           rekord["Azonosító"].ToStrTrim(),
+                                           rekord["Mennyiség"].ToÉrt_Double(),
+                                           rekord["Dátum"].ToÉrt_DaTeTime(),
+                                           rekord["státus"].ToÉrt_Bool(),
+                                           telep);
+                                    Adatok.Add(Adat);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+            return Adatok;
+        }
+
+        public void Rögzítés(string Telephely, Adat_Rezsi_Lista Adat)
+        {
+            try
+            {
+                FájlBeállítás(Telephely);
+                string szöveg = $"INSERT INTO {Táblanév} (azonosító, Mennyiség, dátum, státus ) VALUES (";
+                szöveg += $"'{Adat.Azonosító}', ";
+                szöveg += $"{Adat.Mennyiség.ToStringCult()}, ";
+                szöveg += $"'{Adat.Dátum}', ";
+                szöveg += $"{Adat.Státus})";
+                MyA.ABMódosítás(hely, jelszó, szöveg);
+            }
+            catch (HibásBevittAdat ex)
+            {
+                MessageBox.Show(ex.Message, "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                HibaNapló.Log(ex.Message, this.ToString(), ex.StackTrace, ex.Source, ex.HResult);
+                MessageBox.Show(ex.Message + "\n\n a hiba naplózásra került.", "A program hibára futott", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void Módosítás(string Telephely, Adat_Rezsi_Lista Adat)
+        {
+            try
+            {
+                FájlBeállítás(Telephely);
+
+                string szöveg = $"UPDATE {Táblanév}  SET ";
+                szöveg += $"Mennyiség={Adat.Mennyiség.ToStringCult()}, ";
+                szöveg += $"státus={Adat.Státus}, ";
+                szöveg += $"dátum ='{Adat.Dátum}' ";
+                szöveg += $" WHERE [azonosító]='{Adat.Azonosító}'";
+                MyA.ABMódosítás(hely, jelszó, szöveg);
+            }
+            catch (HibásBevittAdat ex)
+            {
+                MessageBox.Show(ex.Message, "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                HibaNapló.Log(ex.Message, this.ToString(), ex.StackTrace, ex.Source, ex.HResult);
+                MessageBox.Show(ex.Message + "\n\n a hiba naplózásra került.", "A program hibára futott", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void Nagybetűs(string Telephely)
+        {
+            try
+            {
+                FájlBeállítás(Telephely);
+                List<Adat_Rezsi_Lista> Adatok = Lista_Adatok(Telephely);
+                foreach (Adat_Rezsi_Lista rekord in Adatok)
+                {
+                    if (rekord.Azonosító != rekord.Azonosító.ToUpper())
+                    {
+                        Adat_Rezsi_Lista Adat = new Adat_Rezsi_Lista(
+                                                rekord.Azonosító.ToUpper(),
+                                                rekord.Mennyiség,
+                                                rekord.Dátum,
+                                                rekord.Státus);
+                        Rögzítés(Telephely, Adat);
+                        string szöveg = $"DELETE FROM {Táblanév} WHERE Azonosító='{rekord.Azonosító}'";
+                        MyA.ABtörlés(hely, jelszó, szöveg);
+                    }
+                }
+            }
+            catch (HibásBevittAdat ex)
+            {
+                MessageBox.Show(ex.Message, "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                HibaNapló.Log(ex.Message, this.ToString(), ex.StackTrace, ex.Source, ex.HResult);
+                MessageBox.Show(ex.Message + "\n\n a hiba naplózásra került.", "A program hibára futott", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ SqL_Kezelők/Rezsi/SQL_Kezelő_Rezsi_Törzs.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Villamos.Adatszerkezet;
+using MyA = Adatbázis;
+
+namespace Villamos.Kezelők
+{
+    public class SQL_Kezelő_Rezsi_Törzs
+    {
+        readonly string jelszó = "csavarhúzó";
+        readonly string hely = $@"{Application.StartupPath}\Főmérnökség\SQL\Rezsi\rezsitörzs.db";
+        readonly string Táblanév = "törzs";
+
+        public SQL_Kezelő_Rezsi_Törzs()
+        {
+            if (!File.Exists(hely.KönyvSzerk())) Tábla_Létrehozás();
+        }
+
+        private void Tábla_Létrehozás()
+        {
+            try
+            {
+                //A sqlite adatbázisban a következő táblát hoztuk létre
+                // JAVÍTANDÓ:
+                string szöveg = $@"CREATE TABLE {táblanév} (
+                                Azonosító TEXT,
+                            	Honnan TEXT,
+                            	Hova  TEXT,
+                            	Mennyiség REAL,
+                            	Mirehasznál   TEXT,
+                            	Módosította   TEXT,
+                            	Módosításidátum   TEXT,
+                            	Státus    INTEGER);";
+                MyA.SqLite_TáblaLétrehozás(hely.KönyvSzerk(), jelszó, szöveg);
+            }
+            catch (HibásBevittAdat ex)
+            {
+                MessageBox.Show(ex.Message, "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                HibaNapló.Log(ex.Message, this.ToString(), ex.StackTrace, ex.Source, ex.HResult);
+                MessageBox.Show(ex.Message + "\n\n a hiba naplózásra került.", "A program hibára futott", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public List<Adat_Rezsi_Törzs> Lista_Adatok()
+        {
+            List<Adat_Rezsi_Törzs> Adatok = new List<Adat_Rezsi_Törzs>();
+            try
+            {
+                Adatok = MyA.Lista_Adatok(hely, jelszó, táblanév, rekord => new Adat_Rezsi_Törzs(
+                           rekord["Azonosító"].ToStrTrim(),
+                           rekord["Megnevezés"].ToStrTrim(),
+                           rekord["Méret"].ToStrTrim(),
+                           rekord["Státus"].ToÉrt_Int(),
+                           rekord["Csoport"].ToStrTrim()
+                          ));
+            }
+            catch (HibásBevittAdat ex)
+            {
+                MessageBox.Show(ex.Message, "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                HibaNapló.Log(ex.Message, this.ToString(), ex.StackTrace, ex.Source, ex.HResult);
+                MessageBox.Show(ex.Message + "\n\n a hiba naplózásra került.", "A program hibára futott", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return Adatok;
+        }
+
+        public void Rögzítés(Adat_Rezsi_Törzs Adat)
+        {
+            try
+            {
+                string szöveg = $"INSERT INTO {Táblanév} (azonosító, megnevezés, Méret, státus, csoport ) VALUES ";
+                szöveg += "(@Azonosító, @Megnevezés, @Méret, @Státusz, @Csoport)";
+
+                SqliteCommand cmd = new SqliteCommand(szöveg);
+                cmd.Parameters.AddWithValue("@Azonosító ", Adat.Azonosító);
+                cmd.Parameters.AddWithValue("@Megnevezés ", Adat.Megnevezés);
+                cmd.Parameters.AddWithValue("@Méret ", Adat.Méret);
+                cmd.Parameters.AddWithValue("@Státusz ", Adat.Státusz);
+                cmd.Parameters.AddWithValue("@Csoport ", Adat.Csoport);
+
+                MyA.SqLite_Módosítás(hely, jelszó, cmd);
+
+            }
+            catch (HibásBevittAdat ex)
+            {
+                MessageBox.Show(ex.Message, "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                HibaNapló.Log(ex.Message, this.ToString(), ex.StackTrace, ex.Source, ex.HResult);
+                MessageBox.Show(ex.Message + "\n\n a hiba naplózásra került.", "A program hibára futott", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void Módosítás(Adat_Rezsi_Törzs Adat)
+        {
+            try
+            {
+                string szöveg = $"UPDATE {Táblanév} SET ";
+                szöveg += $@"Megnevezés =@Megnevezés, ";
+                szöveg += $@"Méret =@Méret, ";
+                szöveg += $@"Státusz =@Státusz, ";
+                szöveg += $@"Csoport =@Csoport  ";
+                szöveg += $" WHERE azonosító=@Azonosító";
+
+                SqliteCommand cmd = new SqliteCommand(szöveg);
+
+                cmd.Parameters.AddWithValue("@Azonosító ", Adat.Azonosító);
+                cmd.Parameters.AddWithValue("@Megnevezés ", Adat.Megnevezés);
+                cmd.Parameters.AddWithValue("@Méret ", Adat.Méret);
+                cmd.Parameters.AddWithValue("@Státusz ", Adat.Státusz);
+                cmd.Parameters.AddWithValue("@Csoport ", Adat.Csoport);
+
+                MyA.SqLite_Módosítás(hely, jelszó, cmd);
+            }
+            catch (HibásBevittAdat ex)
+            {
+                MessageBox.Show(ex.Message, "Információ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                HibaNapló.Log(ex.Message, this.ToString(), ex.StackTrace, ex.Source, ex.HResult);
+                MessageBox.Show(ex.Message + "\n\n a hiba naplózásra került.", "A program hibára futott", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
