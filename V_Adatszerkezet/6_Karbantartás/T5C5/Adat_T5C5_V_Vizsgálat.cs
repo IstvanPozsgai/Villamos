2e7c34c056305ddeb59e30033ca6f9e4aee0059e
Pozsgai István <117170572+IstvanPozsgai@users.noreply.github.com>
2026-08-20 09:32:41 +0200

    Revert "Idáig jutottam."
    This reverts commit 9b9c7581aa8d7b8a8a547aa58f5d9825f5dcf762.

--- V_Adatszerkezet/6_Karbantartás/T5C5/Adat_T5C5_V_Vizsgálat.cs
+++ V_Adatszerkezet/6_Karbantartás/T5C5/Adat_T5C5_V_Vizsgálat.cs
@@ -33,6 +33,7 @@ namespace Villamos.Adatszerkezet
             TénySzerSz = ténySzerSz;
             TénySzerelvény = ténySzerelvény;
             ElőírtSzerSz = előírtSzerSz;
+            ElőírtSzerelvény1 = előírtSzerelvény1;
             EÍSzerhossz = eÍSzerhossz;
             Státus = státus;
             E3vezénylés = e3vezénylés;
